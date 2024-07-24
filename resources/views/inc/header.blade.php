3a9dd09d68f2a06350f2bd6c827ae97e977c87b4
unknown <diurtorn@gmail.com>
2024-07-24 14:02:02 +0300

    [*]

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -33,9 +33,6 @@
 	<div id="app">
         	<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             	<div class="container">
-                	<a class="navbar-brand" href="{{ url('/') }}">
-                    	Laravel 10 User Roles and Permissions - Tutsmake.com
-                	</a>
                 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 	</button>
