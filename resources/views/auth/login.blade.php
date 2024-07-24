4c8407b501d28fd77c930dbd8fe6ed029eadc108
unknown <diurtorn@gmail.com>
2024-07-24 15:41:26 +0300

    [*]

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 @section('title-block') Кто ты, воин! @endsection
 @section('content')
-<h1>ПУСТОТА</h1>
 <!--<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
