aeca0d6a351485ef4d4b606c1fd9bcdd340a4e7d
unknown <diurtorn@gmail.com>
2024-07-24 20:33:55 +0300

    [*]

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -36,23 +36,7 @@
 			<button class="entrance" id="enterranse">Вход</button>
 
 		@else
-			<li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
-            <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Role</a></li>
-            <li class="nav-item dropdown">
-			<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                {{ Auth::user()->name }} <span class="caret"></span>
-            </a>
-			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
-				<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </div>
-            </li>
+
 		@endguest
         
 
@@ -61,54 +45,14 @@
 
 <!------------------ Окно Входа -------------------------->
 <div class="popup-back" id="popup-back">
-	    <form action="" class="popup-container">
-			<div class="popup-content">
-				<div class="popup-title-wrap">
-					<button class="popup-close close-btn" id="popup-close" title="Закрыть"><img src="{{ asset('img/close.png') }}" alt="Закрыть"></button>
-					<h2 class="popup-title">Вход</h2>
-				</div>
-				<div class="popup-content-wrap">
-					<input type="text" placeholder="email" required class="user-input" ><!--pattern="	\+[0-9]{1,4}[0-9]{1,10}|(.*)@(.*)\.[a-z]{2,5}" title="" oninvalid="setCustomValidity('Пожалуйста, введите 	номер телефона или email')"-->
-					@error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-					<input type="text" placeholder="Введите код" required class="user-input">
-					@error('password')
-                        <span class="invalid-feedback" role="alert">
-                    		<strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-					<div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                    </div>
-					<button type="submit" class="top-menu-btn" id="top-menu-btn">Войти</button>
-					@if (Route::has('password.request'))
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
-                            {{ __('Forgot Your Password?') }}
-                        </a>
-                    @endif
-				</div>
-			</div>
-		</form>
-</div>
-<!--<div class="container">
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="">
+                    <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
@@ -117,7 +61,11 @@
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                         </div>
 
@@ -127,6 +75,11 @@
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                         </div>
 
@@ -148,7 +101,11 @@
                                     {{ __('Login') }}
                                 </button>
 
-
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
                             </div>
                         </div>
                     </form>
@@ -156,4 +113,19 @@
             </div>
         </div>
     </div>
-</div>-->
+</div>
+	    <!--<form action="" class="popup-container">
+			<div class="popup-content">
+				<div class="popup-title-wrap">
+					<button class="popup-close close-btn" id="popup-close" title="Закрыть"><img src="{{ asset('img/close.png') }}" alt="Закрыть"></button>
+					<h2 class="popup-title">Вход</h2>
+				</div>
+				<div class="popup-content-wrap">
+					<input type="text" placeholder="Телефон или email" required class="user-input" >--><!--pattern="	\+[0-9]{1,4}[0-9]{1,10}|(.*)@(.*)\.[a-z]{2,5}" title="" oninvalid="setCustomValidity('Пожалуйста, введите 	номер телефона или email')"-->
+					<!--<input type="text" placeholder="Введите код" required class="user-input">
+					<button type="submit" class="top-menu-btn" id="top-menu-btn">Войти</button>
+				</div>
+			</div>
+		</form>-->
+</div>
+<!---->
