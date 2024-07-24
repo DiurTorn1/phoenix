ec1700c971aa9e40ffce3dd5537f252616c74505
unknown <diurtorn@gmail.com>
2024-07-24 21:08:54 +0300

    [*]

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -68,7 +68,7 @@
 					<button class="popup-close close-btn" id="popup-close" title="Закрыть"><img src="{{ asset('img/close.png') }}" alt="Закрыть"></button>
 					<h2 class="popup-title">Вход</h2>
 				</div>
-				<div class="popup-title-wrap">
+				<div class="popup-content-wrap">
                 	<input id="user-input" type="email" placeholder="Телефон или email" class="user-input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus> 
 					@error('email')
                     	<span class="invalid-feedback" role="alert">
@@ -86,12 +86,13 @@
 						Войти
                     </button>
 				</div>
+				<input class="" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                <label class="" for="remember">
+                        {{ __('Remember Me') }}
+        		</label>
 			</div>
 			
-			<input class="" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                <label class="form-check-label" for="remember">
-                        {{ __('Remember Me') }}
-        	</label>
+
         </form>
 </div>
 	    <!--<form action="" class="popup-container">
