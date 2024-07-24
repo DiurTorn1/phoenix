43f4372767e7eeae45ba0e7d56110662ac0e5bb3
unknown <diurtorn@gmail.com>
2024-07-24 23:53:22 +0300

    [*]

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title-block') Кто ты, воин! @endsection
+@section('title-block') Кто ты, воин? @endsection
 @section('content')
 
 
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title-block') Кто ты, воин! @endsection
+@section('title-block') Кто ты, воин? @endsection
 @section('content')
 <!--<div class="container">
     <div class="row justify-content-center">
