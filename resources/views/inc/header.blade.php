a975c41dbda6a99fe5ffa9b10c8d20058fca9583
unknown <diurtorn@gmail.com>
2024-07-24 20:36:43 +0300

    [*]

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -36,7 +36,23 @@
 			<button class="entrance" id="enterranse">Вход</button>
 
 		@else
-
+		<li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
+            <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Role</a></li>
+            <li class="nav-item dropdown">
+	<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }} <span class="caret"></span>
+            </a>
+			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+				<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </div>
+            </li>
 		@endguest
         
 
