06362ede7d010509c4fca23541d12db7071c8611
unknown <diurtorn@gmail.com>
2024-07-25 15:34:22 +0300

    [*]

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Edit New User</h2>
+            <h2>Добавить нового пользователя</h2>
         </div>
         <div class="pull-right">
             <a class="btn btn-primary" href="{{ route('users.index') }}"> Back</a>
@@ -15,7 +15,7 @@
 
 @if (count($errors) > 0)
   <div class="alert alert-danger">
-    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+    <strong>ООпс!</strong> Слкчилос страшное на выходе.<br><br>
     <ul>
        @foreach ($errors->all() as $error)
          <li>{{ $error }}</li>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Users Management</h2>
+            <h2>Список пользователей</h2>
         </div>
         <div class="pull-right">
             <a class="btn btn-success" href="{{ route('users.create') }}"> Create New User</a>
@@ -53,7 +53,4 @@
 
 
 {!! $data->render() !!}
-
-
-<p class="text-center text-primary"><small>Tutorial by Tutsmake.com</small></p>
 @endsection
