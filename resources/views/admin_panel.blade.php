ca5c7b38de49bacf51bdfdc16364e0df5892e40f
unknown <diurtorn@gmail.com>
2024-07-25 14:13:54 +0300

    [*]

---
+++ resources/views/admin_panel.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('title-block') Панель админа-господина @endsection
+@include('inc.banner')
+@include('inc.header')
+
+       
+
+@section('content')
+
+@endsection
+
+
+</main>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,42 @@
 
        
 @section('content')
+<!------------------ Окно Входа -------------------------->
+<div class="popup-back" id="popup-back">
+        <form method="POST" action="" class="popup-container">
+			<div class="popup-content">
+            	@csrf
+				<div class="popup-title-wrap">
+					<button class="popup-close close-btn" id="popup-close" title="Закрыть"><img src="{{ asset('img/close.png') }}" alt="Закрыть"></button>
+					<h2 class="popup-title">Вход</h2>
+				</div>
+				<div class="popup-content-wrap">
+                	<input id="user-input" type="email" placeholder="Телефон или email" class="user-input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus> 
+					@error('email')
+                    	<span class="invalid-feedback" role="alert">
+                    	<strong>{{ $message }}</strong>
+                    	</span>
+                	@enderror
+				
+                	<input id="password" type="password" placeholder="Телефон или email" class="user-input @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+					@error('password')
+                    	<span class="invalid-feedback" role="alert">
+                    	<strong>{{ $message }}</strong>
+                    	</span>
+                	@enderror
+                    <button type="submit" class="top-menu-btn" id="top-menu-btn">
+						Войти
+                    </button>
+				</div>
+				<input class="" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                <label class="" for="remember">
+                        {{ __('Запомни меня, тварь!!!') }}
+        		</label>
+			</div>
+			
 
+        </form>
+</div>
 
 @endsection
 
