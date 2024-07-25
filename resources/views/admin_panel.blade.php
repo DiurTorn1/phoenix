f3c9e8359f3ee6de0c47194ff9cb1555c4faa0e4
unknown <diurtorn@gmail.com>
2024-07-25 14:39:06 +0300

    [*]

--- resources/views/admin_panel.blade.php
+++ resources/views/admin_panel.blade.php
@@ -5,7 +5,7 @@
 <header class="header admin-header">
 		<div class="left-header">
 			<a href="/" class="close-btn" title="Закрыть" ><img src="img/close.png" alt="Закрыть" width="24" height="24"></a>
-			<h2 class="top-menu-title">Карточка стрима</h2>
+			<h2 class="top-menu-title">Панель админа-господина</h2>
 		</div>
 		<div class="right-heder">
 		</div>
