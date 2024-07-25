2740eea0c9b4b948e40d26dd24b1708dad916f36
unknown <diurtorn@gmail.com>
2024-07-25 14:04:43 +0300

    [*]

--- resources/views/bascet_phenix.blade.php
+++ resources/views/bascet_phenix.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 @section('title-block') ГЛАВНАЯ - Феникс ТВ @endsection
+@include('inc.banner')
+@include('inc.header')
+
+       
+
 @section('content')
 
-  
 @endsection
+
+
+</main>
+
+@include('inc.footer')
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 @section('title-block') ГЛАВНАЯ - Феникс ТВ @endsection
-@include('inc.banner')
+<main class="index-main">
+	    <!-- Главный баннер -->
+	    <div class="banner">
+		    <picture class="banner-img">
+			    <img src="{{ asset('img/banner.png') }}" alt="Феникс ТВ">
+		    </picture>
+		    <div class="banner-text">
+			    <span>Феникс ТВ</span>
+			    <h1>Пространство детского спорта</h1>
+		    </div>
+	    </div>
 @include('inc.header')
 
        
