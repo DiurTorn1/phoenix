11debcfe03be71a642a2846f3912f8e2b404994e
unknown <diurtorn@gmail.com>
2024-07-26 14:04:12 +0300

    [*]

--- public/php/kines.php
+++ public/php/kines.php
@@ -2,10 +2,12 @@
 
 $token = "f49fffe4-42ff-45bb-a03c-3a2eb050226c";
 //setup the request, you can also use CURLOPT_URL
-$ch = curl_init('https://api.kinescope.io/v1/videos?page=1&per_page=25');
+$ch = curl_init('https://api.kinescope.io/v1/videos');
 
 // Returns the data/output as a string instead of raw data
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, "page=1&per_page=100&order=created_at.desc,title.asc&status[]=&folder_id=&project_id=&video_ids[]=&q=&without_folder=true");
 
 //Set your auth headers
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
