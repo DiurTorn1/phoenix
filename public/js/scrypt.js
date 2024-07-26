74476987096d8ec4086372cfec97bf0828f8871f
unknown <diurtorn@gmail.com>
2024-07-26 13:44:15 +0300

    [*]

--- public/js/scrypt.js
+++ public/js/scrypt.js
@@ -20,8 +20,9 @@ $(document).ready(function() {
         $('#popup-back').toggle();
     });
 
-    $("#view-video-btn").on('click', function(){
-        alert("Button clicked!"); // This will display an alert when the button with id "myButton" is clicked
+    //$("#top-menu-btn").on('click', function(){
+        //alert("Button clicked!"); // This will display an alert when the button with id "myButton" is clicked
         //$('#popup-back').toggle();
-    });
+        //alert("Button clicked!");
+    //});
 });
---
+++ public/js/video.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("#view-video-btn").on('click', function(){
+        alert("Button clicked!"); // This will display an alert when the button with id "myButton" is clicked
+        //$('#popup-back').toggle();
+    });
+});
