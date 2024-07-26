463dfd23dee86617d3106a13ae4b4c1b9c48f298
unknown <diurtorn@gmail.com>
2024-07-26 13:30:09 +0300

    [*]

--- public/js/scrypt.js
+++ public/js/scrypt.js
@@ -23,6 +23,5 @@ $(document).ready(function() {
     $("#view-video-btn").on('click', function(){
         alert("Button clicked!"); // This will display an alert when the button with id "myButton" is clicked
         //$('#popup-back').toggle();
-        //alert("Button clicked!");
     });
 });
