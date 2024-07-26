25c2c2541ac29fd7acd26dd24b05e120aea4c9a4
unknown <diurtorn@gmail.com>
2024-07-26 13:58:29 +0300

    [*]

--- public/php/kines.php
+++ public/php/kines.php
@@ -1,5 +1,26 @@
 <?php
 
-echo 'uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu';
+$token = "f49fffe4-42ff-45bb-a03c-3a2eb050226c";
+//setup the request, you can also use CURLOPT_URL
+$ch = curl_init('https://api.kinescope.io/v1/videos');
+
+// Returns the data/output as a string instead of raw data
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+//Set your auth headers
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+   'Content-Type: application/json',
+   'Authorization: Bearer ' . $token
+   ));
+
+// get stringified data/output. See CURLOPT_RETURNTRANSFER
+$data = curl_exec($ch);
+
+// get info about the request
+$info = curl_getinfo($ch);
+// close curl resource to free up system resources
+curl_close($ch);
+
+echo $data;
 
 ?>
