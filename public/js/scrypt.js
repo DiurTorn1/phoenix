85b97d8e8ebdd8770d3bc20a290aa59db3dab780
unknown <diurtorn@gmail.com>
2024-07-28 19:56:08 +0300

    [*]

--- public/js/scrypt.js
+++ public/js/scrypt.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
     $("#enterranse").on('click', function(){
         //alert("Button clicked!"); // This will display an alert when the button with id "myButton" is clicked
         $('#popup-back').toggle();
-    });popup-close
+    });
     
     $("#popup-close").on('click', function(){
         //alert("Button clicked!"); // This will display an alert when the button with id "myButton" is clicked
