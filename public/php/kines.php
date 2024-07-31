883ef5b41f4fb197492189fcae1813e7ac9dfbd6
unknown <diurtorn@gmail.com>
2024-07-31 15:06:29 +0300

    [*]

--- public/php/kines.php
+++ public/php/kines.php
@@ -43,6 +43,9 @@ foreach ($jsonIterator as $key => $val) {
         } 
     //echo "ID:" .$id . "   ,Title: " .$title ."  ,File size: " .$file_size . "   ,Create AT: " .$create_at . "   , URL: " .$url . "    ,Downloa Link: " .$download_link . "................";
     ++$u;
+    for($i = 1; $i < $u; $i++){
+      echo "ID:" .$id[$i] . "   ,Title: " .$title[$i] ."  ,File size: " .$file_size[$i] . "   ,Create AT: " .$create_at[$i] . "   , URL: " .$url[$i] . "    ,Downloa Link: " .$download_link[$i] . "................";
+    }
     //echo $key."    : ".$val . "</br>";
     }
 }
