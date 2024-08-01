9080910303aa087d4828affd350275848021a5e7
unknown <diurtorn@gmail.com>
2024-08-01 18:40:42 +0300

    [*]

--- public/js/video.js
+++ public/js/video.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         $.post('/php/kines.php', function(data)  {
             //console.log(data);
             //$('#res-video-text').text(data);
-            //var jsondata = data;
+            var jsondata = data;
             $.each(jsondata.data, function(k, lic) {
                 //var components = lic.component.length ? lic.component : [lic.component];
                 //$.each(components, function(i, comp) {
