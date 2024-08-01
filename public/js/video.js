0d4786b6baa26e06eb7b982e12d67da6ff1c4a76
unknown <diurtorn@gmail.com>
2024-08-01 19:05:26 +0300

    [*]

--- public/js/video.js
+++ public/js/video.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
             $(data).each(function (i, element) {
                 //result.push(element.attr.id);
-                $('#res-video-text').text(element.data.id);
+                $('#res-video-text').text(element.meta.pagination.page);
         
                 //if (element.children && element.children.length > 0) {
                     //var ids = get_all_ids(element.children);
