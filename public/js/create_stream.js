fbf033c35841f7091742a3c5de02ec0b7076a8e3
unknown <diurtorn@gmail.com>
2024-08-04 23:55:21 +0300

    [*]

--- public/js/create_stream.js
+++ public/js/create_stream.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
     $("#save-st").on('click', function(){
         var main = $('#admin-input-main').val();
+        $.post('/php/create_stream.php', function(data)  {
+            alert("Text:"+data);
+            
+        });
         //var month = label.val('month');
         //var year = label.val('year');
         //var text = label.text();
         //alert(text);
         //alert("Button clicked!"); // This will display an alert when the button with id "myButton" is clicked
         //$('#popup-back').toggle();
-        alert("Text:"+main);
+        
     });
 });
