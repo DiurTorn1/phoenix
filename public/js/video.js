fcc16b59f30f30bd3891eb3ddf34fcea08c87e6a
unknown <diurtorn@gmail.com>
2024-08-04 19:37:43 +0300

    [*]

--- public/js/video.js
+++ public/js/video.js
@@ -31,12 +31,12 @@ $(document).ready(function() {
                     console.log( "\r\nprivacy_domains:" + item.privacy_domains);
                     console.log("\r\nprivacy_share:" + item.privacy_share);
                     console.log("\r\ntags:" + item.tags);
-                    $each(item.poster, function(p, post){
+                    //$each(item.poster, function(p, post){
                         /*console.log("\r\nPoster:\r\nid:" + post.id + "\r\ntype:" + post.type + "\r\nmedia_id:" + post.media_id
                             + "\r\nstatus:" + post.status + "\r\nactive:" + post.active + " \r\noriginal:" + post.original
                             + "\r\nmd:" + post.md + "\r\nsm:" + post.sm + "\r\nxs:" + post.xs + "\r\nfrom_time:" + post.from_time + "\r\nto_time:" + post.to_time
                         );*/
-                    });
+                    //});
                     /*console.log("\r\n----------------------------\r\n");
                     console.log("\r\nadditional_materials:" + chap.additional_materials + "\r\nadditional_materials_enabled:" + chap.additional_materials_enabled + "\r\nplay_link:" + chap.play_link
                         + "\r\nembed_link:" + chap.embed_link + "\r\ncreated_at:" + chap.created_at + "\r\nupdated_at:" + chap.updated_at + "\r\nsubtitles:" + chap.subtitles + "\r\nsubtitles_enabled:" + chap.subtitles_enabled + "\r\n"
