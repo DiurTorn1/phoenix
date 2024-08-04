5d1a8aa1c936f38cba76e55a94d77d70d66e92cd
unknown <diurtorn@gmail.com>
2024-08-04 23:36:55 +0300

    [*]

--- public/js/create_stream.js
+++ public/js/create_stream.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
         var main = $('#admin-input-main');
         //var month = label.val('month');
         //var year = label.val('year');
-        var text = label.text();
+        //var text = label.text();
         //alert(text);
         //alert("Button clicked!"); // This will display an alert when the button with id "myButton" is clicked
         //$('#popup-back').toggle();
-        alert("Text:" + text);
+        alert(label.text());
     });
 });
