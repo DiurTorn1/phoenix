b4110e92ae37c7454b31c5df8d59c9706649e2f3
unknown <diurtorn@gmail.com>
2024-08-07 14:11:50 +0300

    [*]

--- public/php/create_stream.php
+++ public/php/create_stream.php
@@ -2,7 +2,7 @@
 //$json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
 $data = array("name" => "Muuuu","subtitle" => "", "type" => "one-time", "auto_start" => false, "protected" => false, "time_shift" => true,
          "parent_id" => "76be9caa-5ca7-4ac7-947e-4e7042b40632", "reconnect_window" => 600, "scheduled" => array("time" => "2024-08-06T13:05:41.634922Z"),
-         "record" => array("parent_id" => "c21d86ac-7e90-43e7-b825-cbf300951355"),
+         "record" => array("parent_id" => "f5d0756d-2ee5-4531-bde4-7baef6ea5baf"),
          "restreams" => array(), "latency_mode" => "standard");
 $postdata = json_encode($data);
 $token = "f49fffe4-42ff-45bb-a03c-3a2eb050226c";
