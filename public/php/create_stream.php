2d81542e449134091faf9cfb2db54d4b17e7af56
unknown <diurtorn@gmail.com>
2024-08-07 12:09:41 +0300

    [*]

--- public/php/create_stream.php
+++ public/php/create_stream.php
@@ -1,10 +1,17 @@
 <?php
 //$json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
-$data = array("name" => "Muuuu","subtitle" => "Muuu_title", "type" => "recurring", "auto_start" => true, "protected" => false, "time_shift" => false,
-         "parent_id" => "c73909aa-2ebf-4676-bdab-8010ab503076", "reconnect_window" => 1800, "scheduled" => array("time" => "2024-08-07T13:05:41.634922Z"),
-         "record" => array("parent_id" => "c21d86ac-7e90-43e7-b825-cbf300951356"),
-         "restreams" => array("name" => "Restream muuu", "url" => "rtmp://rtmp.kinescope.dev/live", "key" => "c73909aa-2ebf-4676-bdab-8010ab503076", "description" => "Description", "enabled" => false),
-         "latency_mode" => "standard");
+$data = array("data" => array( "id" => "a5659f1e-bb40-442f-a74b-c1eac915442c", "workspace_id" => "f569dae5-2d36-4558-a28e-d2b7d46f7aa2", "parent_id" => "c73909aa-2ebf-4676-bdab-8010ab503075",
+      "name" => "Muu", "subtitle" => "subtitle_muu", "type" => "recurring", "streamkey" => "a6739c5535504feaa45da338dc62f753", "auto_start" => true, "protected" => false, "time_shift" => false, "chat_after_stream" => true, "chat_active" => true,
+      "record" => array( "parent_id" => "c21d86ac-7e90-43e7-b825-cbf300951355" ), "video" => array("presets" => null), "audio" => array("channel_mapping" => null),
+      "restreams" => array("id" => "a4a81173-5e7b-4a98-9d88-0746f920bd09", "event_id" => "a5659f1e-bb40-442f-a74b-c1eac915442c", "name" => "Restream 1", "url" => "rtmp://rtmp.kinescope.dev/live",
+         "key" => "stream key", "status" => "disconnected", "enabled" => false, "description" => "", "created_at" => "2022-11-15T12:25:04.095926Z", "updated_at" => null),
+      "reconnect_window" => 1800, "play_link" => "https://kinescope.dev/200527223", "rtmp_link" => "rtmp://rtmp.kinescope.dev/live", "scheduled" => array("time" => "2024-08-07T00:05:41.634922Z"),
+      "stream" => array( "id" => "a13c213b-912e-4a4a-b4fe-c87475296d2c", "event_id" => "a5659f1e-bb40-442f-a74b-c1eac915442c", "status" => "pending", "started_at" => "2024-08-07T00:05:41.634922Z", "finished_at" => null),
+      "created_at" => "2022-11-15T12:25:04.095926Z", "updated_at" => null, "latency_mode" => "standard",
+      "poster" => array("id" => "ad6f2b26-5135-4360-ac2b-c5adb52ec79e", "type" => "image", "status" => "done", "active" => true, "original" => "https://static.kinescopecdn.net/assets/event_default_poster.png",
+                  "md" => "https://static.kinescopecdn.net/assets/event_default_poster.png", "sm" => "https://static.kinescopecdn.net/assets/event_default_poster.png", "xs" => "https://static.kinescopecdn.net/assets/event_default_poster.png",
+                  "from_time" => 0, "to_time" => 0)
+      ));
 $postdata = json_encode($data);
 $token = "f49fffe4-42ff-45bb-a03c-3a2eb050226c";
 //setup the request, you can also use CURLOPT_URL
@@ -14,7 +21,8 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
    'Content-Type: application/json',
-   'Authorization: Bearer ' . $token
+   'Authorization: Bearer ' . $token,
+   'Content-Length: ' . strlen($postdata)
 ));
 
 $result = curl_exec($ch);
