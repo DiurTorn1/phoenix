ad3c290eeed666aef88cf159731bc1360d218300
unknown <diurtorn@gmail.com>
2024-08-08 19:11:33 +0300

    [*]

--- public/js/get_cardid_stream.js
+++ public/js/get_cardid_stream.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     //$("#jjj").on('click', function(){
         var params = new window.URLSearchParams(window.location.search);
-        console.log(params.get('block-admin-input'));
+        //console.log(params.get('block-admin-input'));
+        $.post('/php/get_cardid_stream.php', { param1: params.get('block-admin-input')}, function(data)  {
+            console.log(data);
+        });
         //alert();
         //$('#admin-input-main').appendVal();
     //});
