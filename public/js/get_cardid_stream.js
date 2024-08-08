cbee56b611060c220107924b214058a8931a8832
unknown <diurtorn@gmail.com>
2024-08-08 19:00:37 +0300

    [*]

--- public/js/get_cardid_stream.js
+++ public/js/get_cardid_stream.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     //$("#jjj").on('click', function(){
         var params = new window.URLSearchParams(window.location.search);
         console.log();
-        alert(params.get('block-admin-input'));
+        //alert();
+        $('#block-admin-input').val(params.get('block-admin-input'));
     //});
     
 });
