62f62f6e5282daa484784a0a45e97d6001b8b741
unknown <diurtorn@gmail.com>
2024-08-08 18:58:56 +0300

    [*]

--- public/js/get_cardid_stream.js
+++ public/js/get_cardid_stream.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $("#jjj").on('click', function(){
+    //$("#jjj").on('click', function(){
         var params = new window.URLSearchParams(window.location.search);
-        console.log(params.get('block-admin-input'));
-        //alert(window.location.href);
-    });
+        console.log();
+        alert(params.get('block-admin-input'));
+    //});
     
 });
