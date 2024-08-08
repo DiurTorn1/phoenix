facf777b8510a8fbc6ab1c5c1a252a39c6c5c4ba
unknown <diurtorn@gmail.com>
2024-08-08 16:09:27 +0300

    [*]

--- public/js/get_stream.js
+++ public/js/get_stream.js
@@ -29,9 +29,8 @@ $(document).ready(function() {
                 } else {
                     vid_sport = "Нет такой группы:" + item.parent_id;
                 }
-                var myDate = item.stream.finished_at;
-                var n=myDate.split("T");
-                console.log(n);
+                var d = jQuery.datepicker.parseDate("yy-mm-ddT00:00:00.000Z", item.stream.finished_at);
+                console.log(d);
                     $("#admin-video-list").append(
                     '<li class="admin-video-item admin-back" id="' + item.id + '">' + 
                         '<div class="admin-video-sort-wrap">' + 
