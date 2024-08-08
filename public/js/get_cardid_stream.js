e51139765166621a24d451a525c1e293a9542979
unknown <diurtorn@gmail.com>
2024-08-08 18:57:27 +0300

    [*]

--- public/js/get_cardid_stream.js
+++ public/js/get_cardid_stream.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     $("#jjj").on('click', function(){
-        alert(window.location.href);
+        var params = new window.URLSearchParams(window.location.search);
+        console.log(params.get('block-admin-input'));
+        //alert(window.location.href);
     });
     
 });
