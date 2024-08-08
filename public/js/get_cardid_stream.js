c44a9ecf223d3836f720085fa7285620df9a0c1a
unknown <diurtorn@gmail.com>
2024-08-08 19:47:02 +0300

    [*]

--- public/js/get_cardid_stream.js
+++ public/js/get_cardid_stream.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     //$("#jjj").on('click', function(){
         var params = new window.URLSearchParams(window.location.search);
         //console.log(params.get('block-admin-input'));
-        $.post('/php/get_cardid_stream.php', { param1: params.get('block-admin-input')}, function(data)  {
+        $.post('/php/get_stream.php', { param1: params.get('block-admin-input')}, function(data)  {
             console.log(data);
         });
         //alert();
--- public/php/get_cardid_stream.php
+++ public/php/get_cardid_stream.php
@@ -1,24 +1,23 @@
 <?php
-    //$json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
-    $data = array();//"name" => "Muuuu","subtitle" => "", "type" => "one-time", "auto_start" => false, "protected" => false, "time_shift" => true,
-         //"parent_id" => "76be9caa-5ca7-4ac7-947e-4e7042b40632", "reconnect_window" => 600, "scheduled" => array("time" => "2024-08-06T13:05:41.634922Z"),
-        // "record" => array("parent_id" => "f5d0756d-2ee5-4531-bde4-7baef6ea5baf"),
-        // "restreams" => array(), "latency_mode" => "standard");
-    $postdata = json_encode($data);
-    $token = "f49fffe4-42ff-45bb-a03c-3a2eb050226c";
-    //setup the request, you can also use CURLOPT_URL
-    $ch = curl_init('https://api.kinescope.io/v2/live/events?67c6bd18-cd33-4287-963a-055d6ee6516c');
-    //curl_setopt($ch, CURLOPT_POST, 1);
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Content-Type: application/json',
-        'Authorization: Bearer ' . $token,
-        //'Content-Length: ' . strlen($postdata)
-    ));
+$token = "f49fffe4-42ff-45bb-a03c-3a2eb050226c";
+//setup the request, you can also use CURLOPT_URL
+$ch = curl_init('https://api.kinescope.io/v2/live/events?page=1&per_page=100&order=created_at.desc,name.asc');//https://api.kinescope.io/v1/videos?page=1&per_page=100&order=created_at.desc,title.asc');
+//curl_setopt($ch, CURLOPT_POST, 1);
+//curl_setopt($ch, CURLOPT_POSTFIELDS, "");
+// Returns the data/output as a string instead of raw data
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt($ch, CURLOPT_POSTFIELDS, "page=1&per_page=100&order=created_at.desc,name.asc");
+//Set your auth headers
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+   'Content-Type: application/json',
+   'Authorization: Bearer ' . $token
+));
 
-    $result = curl_exec($ch);
-    curl_close($ch);
-    echo $result;
-    //echo "I get param1 = ".$_POST['param1'];
+// get stringified data/output. See CURLOPT_RETURNTRANSFER
+$data = curl_exec($ch);
+
+// get info about the request
+$info = curl_getinfo($ch);
+// close curl resource to free up system resources
+curl_close($ch);
 ?>
