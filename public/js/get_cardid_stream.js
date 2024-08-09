634d33e9bab3ddfd6a698f694e6733558c88309e
unknown <diurtorn@gmail.com>
2024-08-09 12:48:45 +0300

    [*]

--- public/js/get_cardid_stream.js
+++ public/js/get_cardid_stream.js
@@ -8,11 +8,11 @@ $(document).ready(function() {
 
             $.each(list,function(i,item){
                 if(item.id == params.get('block-admin-input')){
-                    //console.log("Video inform:\r\n");
-                    //console.log("id: " + item.id + "\r\nworkspace_id: " + item.workspace_id + "\r\nparent_id: " + item.parent_id + "\r\nname: " + item.name + "\r\nsubtitle: " + item.subtitle +
-                        //"\r\ntype: " + item.type + "\r\nstreamkey: " + item.streamkey + "\r\nauto_start: " + item.auto_start + "\r\nprotected: " + item.protected + "\r\ntime_shift: " + item.time_shift); 
-                    //console.log("\r\nRecord: \r\n parent_id: " + item.record.parent_id + "\r\nvideo: \r\n presets: " + item.video.presets + "\r\naudio: \r\n channel_mapping: " + item.audio.channel_mapping + 
-                        //"\r\nreconnect_window:" + item.reconnect_window + "\r\nplay_link: " + item.play_link + "\r\nrtmp_link: " + item.rtmp_link + "\r\nscheduled: \r\n time: " + item.scheduled.time);
+                    console.log("Video inform:\r\n");
+                    console.log(/*"id: " + item.id + "\r\nworkspace_id: " + item.workspace_id + "\r\nparent_id: " + item.parent_id + "\r\nname: " + item.name + "\r\nsubtitle: " + item.subtitle +
+                        "\r\ntype: " + item.type + */"\r\nstreamkey: " + item.streamkey /*+ "\r\nauto_start: " + item.auto_start + "\r\nprotected: " + item.protected + "\r\ntime_shift: " + item.time_shift*/); 
+                    console.log(/*"\r\nRecord: \r\n parent_id: " + item.record.parent_id + "\r\nvideo: \r\n presets: " + item.video.presets + "\r\naudio: \r\n channel_mapping: " + item.audio.channel_mapping + 
+                        //"\r\nreconnect_window:" + item.reconnect_window + "\r\nplay_link: " + item.play_link + */"\r\nrtmp_link: " + item.rtmp_link /*+ "\r\nscheduled: \r\n time: " + item.scheduled.time*/);
                     //console.log("\r\nstream: \r\n id: " + item.stream.id + "\r\nevent_id: " + item.stream.event_id + "\r\nstatus: " + item.stream.status + "\r\nstarted_at: " + item.stream.started_at +  "\r\nfinished_at: " + item.stream.finished_at);
                     //console.log("\r\nchat_after_stream: " + item.chat_after_stream + "\r\nchat_active: " + item.chat_active + "\r\nchat_preview: " + item.chat_preview + "\r\nshow_members: " + item.show_members +
                         //"\r\ncreated_at" + item.created_at + "\r\nupdated_at: " + item.updated_at + "\r\nlatency_mode: " + item.latency_mode + "\r\nallow_chat_links: " + item.allow_chat_links +
@@ -27,13 +27,13 @@ $(document).ready(function() {
                             '<div class="block-admin-sector block-admin-container block-admin-section-item">' +
                                 '<div class="admin-input-box admin-input-wrap">' +
                                     '<label>RTMP сервер</label>' +
-                                    '<input type="text" name="Начало трансляции" class="block-admin-input 		admin-input" placeholder=" " value="1111">' +
+                                    '<input type="text" name="" class="block-admin-input 		admin-input" placeholder=" " value="' + item.rtmp_link + '">' +
                                 '</div>' +
                             '</div>' +
                             '<div class="block-admin-sector block-admin-container block-admin-section-item">' +
                                 '<div class="admin-input-box admin-input-wrap">' +
                                     '<label>Ключ потока</label>' +
-                                    '<input type="text" name="Начало трансляции" class="block-admin-input 		admin-input" placeholder=" " value="1111">' +
+                                    '<input type="text" name="" class="block-admin-input 		admin-input" placeholder=" " value="' + item.streamkey + '">' +
                                 '</div>' +
                             '</div>' +
                         '</div>' +
