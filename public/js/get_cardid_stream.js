613ecbf7bc284122debab4f264f03489aaff07ad
unknown <diurtorn@gmail.com>
2024-08-12 15:57:27 +0300

    [*]

--- public/js/get_cardid_stream.js
+++ public/js/get_cardid_stream.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
         var params = new window.URLSearchParams(window.location.search);
         if(params.get('admin_input_id') != null){
             var id = params.get('admin_input_id');
-            console.log("MSG:" + params.get('block-admin-input'));
+            $('#admin_input_id').val(params.get('admin_input_id'));
+            //console.log("MSG:" + params.get('block-admin-input'));
         }
         //console.log("MSG:" + params.get('block-admin-input'));
         $.post('/php/get_stream.php', function(data)  {
