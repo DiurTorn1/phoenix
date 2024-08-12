f4de7a9248c630d8ff52ff39c3970ba974cd2a5f
unknown <diurtorn@gmail.com>
2024-08-12 15:32:36 +0300

    [*]

--- public/js/scrypt.js
+++ public/js/scrypt.js
@@ -1,7 +1,8 @@
+global.globalVar = "This is global!";
 $(document).ready(function() {
     //$("#publishBtn").click(function(){
     //});
-    global.globalVar = "This is global!";
+    
 
     $("#publishBtn").on('click', function(){
         //alert("Button clicked!"); // This will display an alert when the button with id "myButton" is clicked
