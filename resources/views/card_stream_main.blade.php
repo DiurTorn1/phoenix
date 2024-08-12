8cf39c6afb9cd502a8f09323a93873626363d454
unknown <diurtorn@gmail.com>
2024-08-12 16:14:20 +0300

    [*]

--- resources/views/card_stream_main.blade.php
+++ resources/views/card_stream_main.blade.php
@@ -24,7 +24,6 @@
 	<main class="admin-container">
 	<!-----------------------Меню стрима------------------------------>
 	<div class="admin-menu">
-		<input type="text" required name="ID" class="block_id_input" id="admin_input_id">
 		<nav class="admin-navigation">
 			<ul class="stream-navigation admin-navigation-list admin-back">
 				<li class="admin-navigation-item"><a href="#" class="admin-navigation-link" id="card_stream_link"><img class="admin-menu-icon" src="{{ asset('img/edit.png') }}" alt="Редактировать">Редактировать</a></li>
