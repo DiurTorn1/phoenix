8e86f73b995069804bd7fea81a36829707907e51
unknown <diurtorn@gmail.com>
2024-08-14 12:24:39 +0300

    [*]

--- public/js/create_stream.js
+++ public/js/create_stream.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
     $("#save-st").on('click', function(){
         var main = $('#block-admin-inputct').val();
-        console.log("Select:" + $("#admin_region_selectct option:selected").text());
-        console.log("Season:" + $("#admin_season_seasonct").val());
-        console.log("Turnir:" + $("#admin_turnir_seasonct").val());
-        console.log("Weigth:" + $("#admin_weigth_seasonct").val());
-        console.log("Product:" + $("#admin_product_seasonct").val());
-        console.log("Vid sporta:" + $("#admin_vidsporta_seasonct option:selected").text());
-        console.log("Minframe:" + $("#admin_minframe_seasonct option:selected").index());
-        //$.post('/php/create_stream.php',{name: main}, function(data)  {  //admin_input_select
+        var region = $("#admin_region_selectct option:selected").text();
+        var season = $("#admin_season_seasonct").val();
+        var turnir = $("#admin_turnir_seasonct").val();
+        var weigth = $("#admin_weigth_seasonct").val();
+        var product = $("#admin_product_seasonct").val();
+        var vid_sporta = $("#admin_vidsporta_seasonct option:selected").text();
+        var minframe = $("#admin_minframe_seasonct option:selected").index();
+        $.post('/php/create_stream.php',{name: main, region: region, season: season, turnir: turnir, weigth: weigth, product: product, vid_sporta: vid_sporta, minframe: minframe}, function(data)  {  //admin_input_select
             //alert("Text:"+data);admin_input_seasonct
-            //console.log(main);
+            console.log(data);
             
-        //});
+        });
         //window.location.href='/stream';//?block-admin-input='+id;
         //var month = label.val('month');
         //var year = label.val('year');
