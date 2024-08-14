9f7d30c5be961953c0fc452d9077aebd68d046f6
unknown <diurtorn@gmail.com>
2024-08-14 14:01:36 +0300

    [*]

--- public/js/create_stream.js
+++ public/js/create_stream.js
@@ -14,15 +14,16 @@ $(document).ready(function() {
             //alert("Text:"+data);admin_input_seasonct   ditetime_start_at
             //console.log(data);
             var output = $.parseJSON(data);
-            var list = output.data;
-            $('#admin_link_playct').val(output.play_link);
-            $.each(list,function(i,item){
-                console.log("Video inform:\r\n");
-                console.log("data:\r\n" + "id: " + item.id + "\r\nworkspace_id: " + item.workspace_id + "\r\nparent_id: " + item.parent_id + "\r\nname: " + item.name +
-                    "\r\nsubtitle: " + item.subtitle + "\r\ntype: " + item.type + "\r\nstreamkey: " + item.streamkey + "\r\nauto_start: " + item.auto_start + "\r\nprotected: " + item.protected +
-                    "\r\ntime_shift: " + item.time_shift); 
+            console.log(output);
+            //var list = output.data;
+            // $('#admin_link_playct').val(output.play_link);
+            //$.each(list,function(i,item){
+                //console.log("Video inform:\r\n");
+                //console.log("data:\r\n" + "id: " + item.id + "\r\nworkspace_id: " + item.workspace_id + "\r\nparent_id: " + item.parent_id + "\r\nname: " + item.name +
+                    //"\r\nsubtitle: " + item.subtitle + "\r\ntype: " + item.type + "\r\nstreamkey: " + item.streamkey + "\r\nauto_start: " + item.auto_start + "\r\nprotected: " + item.protected +
+                    //"\r\ntime_shift: " + item.time_shift); 
                 
-            });
+            //});
             
         });
         //window.location.href='/stream';//?block-admin-input='+id;
