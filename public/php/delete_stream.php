7b3262cb57e49b1397863d8a244f01a688bb5cea
unknown <diurtorn@gmail.com>
2024-08-16 00:10:03 +0300

    [*]

--- public/php/delete_stream.php
+++ public/php/delete_stream.php
@@ -2,4 +2,29 @@
 $json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
 $token = "f49fffe4-42ff-45bb-a03c-3a2eb050226c";
 
-echo $_POST['id'];
+//setup the request, you can also use CURLOPT_URL
+$ch = curl_init('https://api.kinescope.io/v2/live/events/' . $_POST['id']);//https://api.kinescope.io/v1/videos?page=1&per_page=100&order=created_at.desc,title.asc');
+//curl_setopt($ch, CURLOPT_POST, 1);
+//curl_setopt($ch, CURLOPT_POSTFIELDS, "");
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+//curl_setopt($ch, CURLOPT_POST, 1);
+//curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+// Returns the data/output as a string instead of raw data
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt($ch, CURLOPT_POSTFIELDS, "page=1&per_page=100&order=created_at.desc,name.asc");
+//Set your auth headers
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  // 'Content-Type: application/json',
+  // 'Authorization: Bearer ' . $token
+//));
+
+// get stringified data/output. See CURLOPT_RETURNTRANSFER
+$data = curl_exec($ch);
+
+// get info about the request
+$info = curl_getinfo($ch);
+// close curl resource to free up system resources   763cf3d3-f251-4e89-8a5b-306fe9b83da8   https://api.kinescope.io/v2/live/events/:event_id
+curl_close($ch);
+
+echo $data;
+//echo $_POST['id'];
