f30a860b7629b46f4e4a5bfebf8c87378f8e837d
unknown <diurtorn@gmail.com>
2024-08-15 13:57:09 +0300

    [*]

--- public/php/upload_stream.php
+++ public/php/upload_stream.php
@@ -3,7 +3,7 @@
 
 $token = "f49fffe4-42ff-45bb-a03c-3a2eb050226c";
 //setup the request, you can also use CURLOPT_URL
-$ch = curl_init('https://api.kinescope.io/v2/live/events/:763cf3d3-f251-4e89-8a5b-306fe9b83da8');//https://api.kinescope.io/v1/videos?page=1&per_page=100&order=created_at.desc,title.asc');
+$ch = curl_init('https://api.kinescope.io/v2/live/events/763cf3d3-f251-4e89-8a5b-306fe9b83da8');//https://api.kinescope.io/v1/videos?page=1&per_page=100&order=created_at.desc,title.asc');
 //curl_setopt($ch, CURLOPT_POST, 1);
 //curl_setopt($ch, CURLOPT_POSTFIELDS, "");
 // Returns the data/output as a string instead of raw data
