b57b6e10dbbc9a67db6f6b8d1300e51608a7e971
unknown <diurtorn@gmail.com>
2024-08-15 15:42:59 +0300

    [*]

--- public/js/get_cardid_stream.js
+++ public/js/get_cardid_stream.js
@@ -105,17 +105,17 @@ $(document).ready(function() {
             var output2 = $.parseJSON(data);
             var list2 = output2.data;
             $.each(list2,function(i,item){
-                console.log("Video inform:\r\n");
-                console.log("id: " + item.id + "\r\nworkspace_id: " + item.workspace_id + "\r\nparent_id: " + item.parent_id + "\r\nname: " + item.name + "\r\nsubtitle: " + item.subtitle +
-                    "\r\ntype: " + item.type + "\r\nstreamkey: " + item.streamkey + "\r\nauto_start: " + item.auto_start + "\r\nprotected: " + item.protected + "\r\ntime_shift: " + item.time_shift); 
-                console.log("\r\nRecord: \r\n parent_id: " + item.record.parent_id + "\r\nvideo: \r\n presets: " + item.video.presets + "\r\naudio: \r\n channel_mapping: " + item.audio.channel_mapping + 
-                    "\r\nreconnect_window:" + item.reconnect_window + "\r\nplay_link: " + item.play_link + "\r\nrtmp_link: " + item.rtmp_link + "\r\nscheduled: \r\n time: " + item.scheduled.time);
-                console.log("\r\nstream: \r\n id: " + item.stream.id + "\r\nevent_id: " + item.stream.event_id + "\r\nstatus: " + item.stream.status + "\r\nstarted_at: " + item.stream.started_at +  "\r\nfinished_at: " + item.stream.finished_at);
-                console.log("\r\nchat_after_stream: " + item.chat_after_stream + "\r\nchat_active: " + item.chat_active + "\r\nchat_preview: " + item.chat_preview + "\r\nshow_members: " + item.show_members +
-                    "\r\ncreated_at" + item.created_at + "\r\nupdated_at: " + item.updated_at + "\r\nlatency_mode: " + item.latency_mode + "\r\nallow_chat_links: " + item.allow_chat_links +
-                    "\r\nmoderators: " + item.moderators);
-                console.log("\r\nposter \r\nid:" + item.poster.id + "\r\ntype: " + item.poster.type + "\r\nstatus" + item.poster.status + "\r\nactive: " + item.poster.active + "\r\noriginal: " + 
-                    "\r\nmd: " + item.poster.md + "\r\nsm: " + item.poster.sm + "\r\nxs: " + item.poster.xs +"\r\nfrom_time" + item.poster.from_time + "\r\nto_time" + item.poster.to_time);
+                //console.log("Video inform:\r\n");
+                //console.log("id: " + item.id + "\r\nworkspace_id: " + item.workspace_id + "\r\nparent_id: " + item.parent_id + "\r\nname: " + item.name + "\r\nsubtitle: " + item.subtitle +
+                    //"\r\ntype: " + item.type + "\r\nstreamkey: " + item.streamkey + "\r\nauto_start: " + item.auto_start + "\r\nprotected: " + item.protected + "\r\ntime_shift: " + item.time_shift); 
+                //console.log("\r\nRecord: \r\n parent_id: " + item.record.parent_id + "\r\nvideo: \r\n presets: " + item.video.presets + "\r\naudio: \r\n channel_mapping: " + item.audio.channel_mapping + 
+                    //"\r\nreconnect_window:" + item.reconnect_window + "\r\nplay_link: " + item.play_link + "\r\nrtmp_link: " + item.rtmp_link + "\r\nscheduled: \r\n time: " + item.scheduled.time);
+                //console.log("\r\nstream: \r\n id: " + item.stream.id + "\r\nevent_id: " + item.stream.event_id + "\r\nstatus: " + item.stream.status + "\r\nstarted_at: " + item.stream.started_at +  "\r\nfinished_at: " + item.stream.finished_at);
+                //console.log("\r\nchat_after_stream: " + item.chat_after_stream + "\r\nchat_active: " + item.chat_active + "\r\nchat_preview: " + item.chat_preview + "\r\nshow_members: " + item.show_members +
+                    //"\r\ncreated_at" + item.created_at + "\r\nupdated_at: " + item.updated_at + "\r\nlatency_mode: " + item.latency_mode + "\r\nallow_chat_links: " + item.allow_chat_links +
+                    //"\r\nmoderators: " + item.moderators);
+                //console.log("\r\nposter \r\nid:" + item.poster.id + "\r\ntype: " + item.poster.type + "\r\nstatus" + item.poster.status + "\r\nactive: " + item.poster.active + "\r\noriginal: " + 
+                    //"\r\nmd: " + item.poster.md + "\r\nsm: " + item.poster.sm + "\r\nxs: " + item.poster.xs +"\r\nfrom_time" + item.poster.from_time + "\r\nto_time" + item.poster.to_time);
                 //if(item.id == params.get('admin_input_id')){
                     //alert("Редактирование успешно!");
                     //window.location.href='/card_stream?admin_input_id='+params.get('admin_input_id');
@@ -145,3 +145,58 @@ $(document).ready(function() {
     });
     
 });
+/**
+ * "id": "763cf3d3-f251-4e89-8a5b-306fe9b83da8",
+  "workspace_id": "eb7874ed-0833-4b89-b2f1-65d26ea6643f",
+  "parent_id": "a08220b1-2640-4d65-b51b-cd4d2837741e",
+  "name": "Game_2",
+  "subtitle": "20.24-2025\u0026МячМячМяч\u0026Ю11\u0026Баскетбол\u0026 \u0026 \u0026Все регионы",
+  "type": "one-time",
+  "streamkey": "34e411524f98436b9aed3f074d99a2a7",
+  "auto_start": false,
+  "protected": false,
+  "time_shift": true,
+  "record": {
+   "parent_id": "f5d0756d-2ee5-4531-bde4-7baef6ea5baf"
+  },
+  "video": {
+   "presets": null
+  },
+  "audio": {
+   "channel_mapping": null
+  },
+  "restreams": [],
+  "reconnect_window": 600,
+  "play_link": "https://kinescope.io/jhRH4DBNS2SaXzj1D6Bwsq",
+  "rtmp_link": "rtmp://rtmp.kinescope.io/live",
+  "scheduled": {
+   "time": "2024-08-14T13:44:00Z"
+  },
+  "stream": {
+   "id": "9b58495e-f2e3-43f9-8c15-75fb92737257",
+   "event_id": "763cf3d3-f251-4e89-8a5b-306fe9b83da8",
+   "status": "pending",
+   "started_at": "2024-08-14T13:44:00Z",
+   "finished_at": null
+  },
+  "chat_after_stream": true,
+  "chat_active": false,
+  "chat_preview": true,
+  "show_members": true,
+  "created_at": "2024-08-14T10:44:42.746261Z",
+  "updated_at": null,
+  "latency_mode": "standard",
+  "allow_chat_links": false,
+  "moderators": null,
+  "poster": {
+   "id": "2da5947f-9131-4b22-aa64-f15d4ee70b0c",
+   "type": "image",
+   "status": "done",
+   "active": true,
+   "original": "https://static.kinescopecdn.net/assets/event_default_poster.png",
+   "md": "https://static.kinescopecdn.net/assets/event_default_poster.png",
+   "sm": "https://static.kinescopecdn.net/assets/event_default_poster.png",
+   "xs": "https://static.kinescopecdn.net/assets/event_default_poster.png",
+   "from_time": 0,
+   "to_time": 0
+ */
