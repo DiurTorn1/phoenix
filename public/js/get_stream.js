4c1f48e9dc10414a7efb0b539567bdb763539cfb
unknown <diurtorn@gmail.com>
2024-08-15 23:51:50 +0300

    [*]

--- public/js/get_stream.js
+++ public/js/get_stream.js
@@ -92,7 +92,7 @@ $(document).ready(function() {
         $("#delete_stream_checkbox").on('click', function() {
             var ch1 = $("#admin-vl1").is(':checked');
             var id = $("#admin-vl1").parent().parent().attr('id');
-            alert("Delete stream: " + ch1 + ",id:" + id);
+            console.log("Delete stream: " + ch1 + ",id:" + id);
         });
         //var month = label.val('month');
         //var year = label.val('year');
