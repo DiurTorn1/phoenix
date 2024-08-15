c030637b4a0804fe1919150ef2941079f5ef6dcc
unknown <diurtorn@gmail.com>
2024-08-15 14:35:05 +0300

    [*]

--- public/php/upload_stream.php
+++ public/php/upload_stream.php
@@ -6,8 +6,9 @@ $token = "f49fffe4-42ff-45bb-a03c-3a2eb050226c";
 
 $data = array( "name" => "Update_Game", "subtitle" => "2024-2025&МячNach&Ю11&Баскетбол& & &Все регионы", "auto_start" => false, "protected" => false, "time_shift" => false,
                 "reconnect_window" => 1800, "chat_after_stream"=> false, "chat_active" => false, "scheduled" => array("time" => "2024-08-19T00:05:00.000000Z"),
-                "record" => array("parent_id" => "a08220b1-2640-4d65-b51b-cd4d2837741e"), "latency_mode" => "standard", "moderators" => ["45b48179-a776-479d-9cb4-3d9158be9a56"], "show_members" => true,
+                "record" => array("parent_id" => "a08220b1-2640-4d65-b51b-cd4d2837741e"), "latency_mode" => "standard", "moderators" => null, "show_members" => true,
                 "chat_preview" => false);
+                
 $postdata = json_encode($data);
 
 
@@ -15,9 +16,9 @@ $postdata = json_encode($data);
 $ch = curl_init('https://api.kinescope.io/v2/live/events/' . $_POST['id']);//https://api.kinescope.io/v1/videos?page=1&per_page=100&order=created_at.desc,title.asc');
 //curl_setopt($ch, CURLOPT_POST, 1);
 //curl_setopt($ch, CURLOPT_POSTFIELDS, "");
-//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
 //curl_setopt($ch, CURLOPT_POST, 1);
-//curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
 // Returns the data/output as a string instead of raw data
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 //curl_setopt($ch, CURLOPT_POSTFIELDS, "page=1&per_page=100&order=created_at.desc,name.asc");
@@ -58,4 +59,60 @@ curl --location --request PUT 'https://api.kinescope.io/v2/live/events/:event_id
     "moderators": ["45b48179-a776-479d-9cb4-3d9158be9a56"],
     "show_members": true,
     "chat_preview": true
+
+    "data": {
+  "id": "763cf3d3-f251-4e89-8a5b-306fe9b83da8",
+  "workspace_id": "eb7874ed-0833-4b89-b2f1-65d26ea6643f",
+  "parent_id": "a08220b1-2640-4d65-b51b-cd4d2837741e",
+  "name": "Game_2",
+  "subtitle": "20.24-2025\u0026МячМячМяч\u0026Ю11\u0026Баскетбол\u0026 \u0026 \u0026Все регионы",
+  "type": "one-time",
+  "streamkey": "34e411524f98436b9aed3f074d99a2a7",
+  "auto_start": false,
+  "protected": false,
+  "time_shift": true,
+  "record": {
+   "parent_id": "f5d0756d-2ee5-4531-bde4-7baef6ea5baf"
+  },
+  "video": {
+   "presets": null
+  },
+  "audio": {
+   "channel_mapping": null
+  },
+  "restreams": [],
+  "reconnect_window": 600,
+  "play_link": "https://kinescope.io/jhRH4DBNS2SaXzj1D6Bwsq",
+  "rtmp_link": "rtmp://rtmp.kinescope.io/live",
+  "scheduled": {
+   "time": "2024-08-14T13:44:00Z"
+  },
+  "stream": {
+   "id": "9b58495e-f2e3-43f9-8c15-75fb92737257",
+   "event_id": "763cf3d3-f251-4e89-8a5b-306fe9b83da8",
+   "status": "pending",
+   "started_at": "2024-08-14T13:44:00Z",
+   "finished_at": null
+  },
+  "chat_after_stream": true,
+  "chat_active": false,
+  "chat_preview": true,
+  "show_members": true,
+  "created_at": "2024-08-14T10:44:42.746261Z",
+  "updated_at": null,
+  "latency_mode": "standard",
+  "allow_chat_links": false,
+  "moderators": null,
+  "poster": {
+   "id": "2da5947f-9131-4b22-aa64-f15d4ee70b0c",
+   "type": "image",
+   "status": "done",
+   "active": true,
+   "original": "https://static.kinescopecdn.net/assets/event_default_poster.png",
+   "md": "https://static.kinescopecdn.net/assets/event_default_poster.png",
+   "sm": "https://static.kinescopecdn.net/assets/event_default_poster.png",
+   "xs": "https://static.kinescopecdn.net/assets/event_default_poster.png",
+   "from_time": 0,
+   "to_time": 0
+  }
 }' */
