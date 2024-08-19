eb24d5d92ed398dde96fb060080380a91e38295c
unknown <diurtorn@gmail.com>
2024-08-19 15:44:43 +0300

    [*]

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,6 +14,7 @@ class Product extends Model
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password'
+        'name', 'detail', 'datetime_start', 'datetime_stop', 'type' => 'required', 'country', 'valute', 'period_job', 'prob_period', 'prob_price',
+        'datetime_start_sell', 'price', 'old_price', 'datetime_start_access', 'datetime_stop_access', 'datetime_stop_sell', 'img_main', 'img_banner', 'img_video_promo', 'video_promo',
     ];
 }
--- database/migrations/create_products_table.php
+++ database/migrations/create_products_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
             $table->timestamps();
         });
     }
