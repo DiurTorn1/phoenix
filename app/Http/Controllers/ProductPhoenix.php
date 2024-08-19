d1d3e30ce68991f80fa228c57c9b35ee5c157837
unknown <diurtorn@gmail.com>
2024-08-19 16:04:29 +0300

    [*]

--- app/Http/Controllers/ProductPhoenix.php
+++ app/Http/Controllers/ProductPhoenix.php
@@ -6,12 +6,27 @@ use Illuminate\Http\Request;
 
 class ProductPhoenix extends Controller
 {
+        /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    function __construct()
+    {
+         $this->middleware('permission:product-list|product-create|product-edit|product-delete', ['only' => ['index','show']]);
+         $this->middleware('permission:product-create', ['only' => ['create','store']]);
+         $this->middleware('permission:product-edit', ['only' => ['edit','update']]);
+         $this->middleware('permission:product-delete', ['only' => ['destroy']]);
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
         //
+        $products = Product::latest()->paginate(5);
+        return view('product_admin',compact('products'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
     /**
@@ -28,6 +43,33 @@ class ProductPhoenix extends Controller
     public function store(Request $request)
     {
         //
+        request()->validate([
+            'name' => 'required',
+            'detail' => 'required',
+            'datetime_start' => 'required',
+            'datetime_stop' => 'required',
+            'type' => 'required',
+            'country' => 'required',
+            'valute' => 'required',
+            'period_job' => 'required',
+            'prob_period' => 'required',
+            'prob_price' => 'required',
+            'datetime_start_sell' => 'required',
+            'price' => 'required',
+            'old_price' => 'required',
+            'datetime_start_access' => 'required',
+            'datetime_stop_access' => 'required',
+            'datetime_stop_sell' => 'required',
+            'img_main' => 'required',
+            'img_banner' => 'required',
+            'img_video_promo' => 'required',
+            'video_promo' => 'required',
+        ]);
+    
+        Product::create($request->all());
+    
+        return redirect()->route('products.index')
+                        ->with('success','Product created successfully.');
     }
 
     /**
