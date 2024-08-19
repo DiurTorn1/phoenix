f0308d8bfdb55eba2c7273a73c19fe1528130163
unknown <diurtorn@gmail.com>
2024-08-19 16:35:42 +0300

    [*]

--- resources/views/product_admin.blade.php
+++ resources/views/product_admin.blade.php
@@ -68,7 +68,7 @@
 		</div>
 
 		<ul class="admin-video-list admin-video-sort prod-mr">
-			@foreach($Product as $prod)
+			@foreach($products as $product)
 
 			@endforeach
 			<!--<li class="admin-video-item admin-back">
