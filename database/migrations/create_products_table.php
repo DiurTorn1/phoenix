90879facd1f891d48dbd754fb406b68e23b43406
unknown <diurtorn@gmail.com>
2024-08-22 18:21:47 +0300

    [*]

--- database/migrations/create_products_table.php
+++ database/migrations/create_products_table.php
@@ -14,6 +14,25 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('detail');
+            $table->timestamps('datetime_start');
+            $table->timestamps('datetime_stop');
+            $table->string('type');
+            $table->string('country');
+            $table->string('valute');
+            $table->integer('period_job');
+            $table->integer('prob_period');
+            $table->integer('prob_price');
+            $table->timestamps('datetime_start_sell');
+            $table->integer('price');
+            $table->integer('old_price');
+            $table->timestamps('datetime_start_access');
+            $table->timestamps('datetime_stop_access');
+            $table->timestamps('datetime_stop_sell');
+            $table->string('img_main');
+            $table->string('img_banner');
+            $table->string('img_video_promo');
+            $table->string('video_promo');
             $table->timestamps();
         });
     }
