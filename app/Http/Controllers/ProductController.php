dc6673a98010839d3eca4b4e360cf797d092e08b
unknown <diurtorn@gmail.com>
2024-08-22 19:43:09 +0300

    [*]

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -27,7 +27,7 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::latest()->paginate(5);
-        return view('products.index',compact('products'))
+        return view('product_admin',compact('products'))
             ->with('i', (request()->input('page', 1) - 1) * 5);
     }
     
