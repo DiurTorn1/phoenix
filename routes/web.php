eb1c311f95a858fae9f792e0a49e7ca4b82f416c
unknown <diurtorn@gmail.com>
2024-08-22 18:55:33 +0300

    [*]

--- routes/web.php
+++ routes/web.php
@@ -96,7 +96,7 @@ Route::get('/video_add', function () {
 Route::group(['middleware' => ['auth']], function() {
     Route::resource('roles', RoleController::class);
     Route::resource('users', UserController::class);
-    Route::resource('products', ProductPhoenix::class);
+    Route::resource('products', ProductController::class);
 });
 
 Auth::routes();
