fe9e8aeb1d9b18070943ffc5d54d9fd659f84dcb
unknown <diurtorn@gmail.com>
2024-08-22 17:49:58 +0300

    [*]

--- routes/web.php
+++ routes/web.php
@@ -74,9 +74,11 @@ Route::get('/product_admin', function () {
     return view('product_admin');
 });
 
-Route::get('/product_add', [App\Http\Controllers\ProductPhoenix::class, 'index']);//ProductPhoenix::class .'@index');//->name('Product');//'ProductPhoenix@index');//->name('Product');//function () {// 
+Route::get('/product_add',function () {// ProductPhoenix::class .'@index')->name('products');//'ProductPhoenix@index');//->name('Product');//// 
     //return view('product_add');
-//});
+    $products = Product::all;//latest()->paginate(5);
+    return view('product_admin')->with(compact('products'))
+});
 
 Route::get('/promocode', function () {
     return view('promocode');
