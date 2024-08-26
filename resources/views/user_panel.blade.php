d3f8abf276e30a782e15f7b0d76d5f6bd4908d54
unknown <diurtorn@gmail.com>
2024-08-26 17:56:58 +0300

    [*]

--- resources/views/user_panel.blade.php
+++ resources/views/user_panel.blade.php
@@ -7,8 +7,6 @@
 
 
 @else
-	@if(!empty(Auth::user()->getRoleNames()))
-    @foreach( Auth::user()->getRoleNames() as $v)
         <!-------------------------------Окно НАСТРОЕК Пользователя------------------------------->
   	<div class="popup-back"> 
 		<div class="popup-container">
@@ -175,8 +173,6 @@
 
 	<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="user_menu.js"></script>-->
-    @endforeach
-    @endif
 @endguest
 
 @endsection
