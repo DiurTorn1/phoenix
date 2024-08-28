8b4122039d00c3092202273285357a1e561eee32
unknown <diurtorn@gmail.com>
2024-08-28 19:24:12 +0300

    [*]

--- public/php/product_add.php
+++ public/php/product_add.php
@@ -30,7 +30,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["detail"]. "<br>";
     }
 } else {
     echo "0 results";
