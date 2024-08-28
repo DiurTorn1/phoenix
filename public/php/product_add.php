5692f42d14e180900a22231836b75e4db5bba20b
unknown <diurtorn@gmail.com>
2024-08-28 18:19:08 +0300

    [*]

--- public/php/product_add.php
+++ public/php/product_add.php
@@ -24,7 +24,7 @@ if (!$conn) {
 $query = "INSERT INTO products(name, detail, datetime_start, datetime_stop, type, country, valute, period_job, prob_period, prob_price, datetime_start_sell, price, old_price, datetime_start_access, datetime_stop_access, datetime_stop_sell, img_main, img_banner, img_video_promo, video_promo) VALUES ('name', 'detail', 'state', 'sell_all', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'type', 'country', 'valute', 'period_job', 'prob_period', 'prob_price', '2024-08-28 15:37:32', '120', '100', '2024-08-28 15:37:32', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'img_main', 'img_banner', 'img_video_promo', 'video_promo')";
 
 // Execute the query
-//$result = mysqli_query($conn, $query);
+$result = mysqli_query($conn, $query);
 
 // Check for success or error
 //if ($result) {
