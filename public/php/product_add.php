3c40aa3c0f8cc3a4ec6afb5085fbf7865a891cb8
unknown <diurtorn@gmail.com>
2024-08-28 19:18:47 +0300

    [*]

--- public/php/product_add.php
+++ public/php/product_add.php
@@ -1,44 +1,16 @@
 <?php
 
-//$servername = "localhost";
-//$username = "diur_admin";
-//$password = "%v~kfAt5W#d82sNCWZiF";
-//$dbname = "phoenix_fun";
-
-
-$host = 'localhost';
-$username = 'diur_admin';
-$password = '%v~kfAt5W#d82sNCWZiF';
-$database = 'phoenix_fun';
-
-$conn = mysqli_connect($host, $username, $password, $database);
-
-// Check if the connection was successful
-if (!$conn) {
-    die('Failed to connect to MySQL: ' . mysqli_connect_error());
-} else {
-    echo "test";
-}
-
-// Prepare the INSERT query
-$query = "INSERT INTO products(name, detail, datetime_start, datetime_stop, type, country, valute, period_job, prob_period, prob_price, datetime_start_sell, price, old_price, datetime_start_access, datetime_stop_access, datetime_stop_sell, img_main, img_banner, img_video_promo, video_promo) VALUES ('name', 'detail', 'state', 'sell_all', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'type', 'country', 'valute', 'period_job', 'prob_period', 'prob_price', '2024-08-28 15:37:32', '120', '100', '2024-08-28 15:37:32', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'img_main', 'img_banner', 'img_video_promo', 'video_promo')";
-
-// Execute the query
-$result = mysqli_query($conn, $query);
-
-// Check for success or error
-//if ($result) {
-    //echo "Data inserted successfully!";
-//} else {
-    //echo "Error: " . mysqli_error($conn);
-//}
+$servername = "localhost";
+$username = "diur_admin";
+$password = "%v~kfAt5W#d82sNCWZiF";
+$dbname = "phoenix_fun";
 
 // Create connection
-//$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-//if ($conn->connect_error) {
-  //die("Connection failed: " . $conn->connect_error);
-//}
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
 //$sql = "INSERT INTO `products`(`id`, `name`, `detail`, `datetime_start`, `datetime_stop`, `type`, `country`, `valute`, `period_job`, `prob_period`, `prob_price`, `datetime_start_sell`, `price`, `old_price`, `datetime_start_access`, `datetime_stop_access`, `datetime_stop_sell`, `img_main`, `img_banner`, `img_video_promo`, `video_promo`) VALUES ('name', 'detail', 'state', 'sell_all', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'type', 'country', 'valute', 'period_job', 'prob_period', 'prob_price', '2024-08-28 15:37:32', '120', '100', '2024-08-28 15:37:32', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'img_main', 'img_banner', 'img_video_promo', 'video_promo')";
 //        VALUES ('name', 'detail', 'state', 'sell_all', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'type', 'country', 'valute', 'period_job', 'prob_period', 'prob_price',
@@ -52,5 +24,16 @@ $result = mysqli_query($conn, $query);
 //}
 
 //$conn->close();
-mysqli_close($conn);
+$sql = "SELECT id, name, detail FROM products";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 //echo "test";
