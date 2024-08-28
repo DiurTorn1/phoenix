ce093fb491da389598721f0143bc77c62a5b2109
unknown <diurtorn@gmail.com>
2024-08-28 18:11:39 +0300

    [*]

--- public/php/product_add.php
+++ public/php/product_add.php
@@ -1,27 +1,42 @@
 <?php
 
-$servername = "localhost";
-$username = "diur_admin";
-$password = "%v~kfAt5W#d82sNCWZiF";
-$dbname = "phoenix_fun";
+//$servername = "localhost";
+//$username = "diur_admin";
+//$password = "%v~kfAt5W#d82sNCWZiF";
+//$dbname = "phoenix_fun";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+
+$host = 'localhost';
+$username = 'diur_admin';
+$password = '%v~kfAt5W#d82sNCWZiF';
+$database = 'phoenix_fun';
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Check if the connection was successful
+if (!$conn) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
+} else {
+    echo "test";
 }
+// Create connection
+//$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+//if ($conn->connect_error) {
+  //die("Connection failed: " . $conn->connect_error);
+//}
 
-$sql = "INSERT INTO `products`(`id`, `name`, `detail`, `datetime_start`, `datetime_stop`, `type`, `country`, `valute`, `period_job`, `prob_period`, `prob_price`, `datetime_start_sell`, `price`, `old_price`, `datetime_start_access`, `datetime_stop_access`, `datetime_stop_sell`, `img_main`, `img_banner`, `img_video_promo`, `video_promo`) VALUES ('name', 'detail', 'state', 'sell_all', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'type', 'country', 'valute', 'period_job', 'prob_period', 'prob_price', '2024-08-28 15:37:32', '120', '100', '2024-08-28 15:37:32', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'img_main', 'img_banner', 'img_video_promo', 'video_promo')";
+//$sql = "INSERT INTO `products`(`id`, `name`, `detail`, `datetime_start`, `datetime_stop`, `type`, `country`, `valute`, `period_job`, `prob_period`, `prob_price`, `datetime_start_sell`, `price`, `old_price`, `datetime_start_access`, `datetime_stop_access`, `datetime_stop_sell`, `img_main`, `img_banner`, `img_video_promo`, `video_promo`) VALUES ('name', 'detail', 'state', 'sell_all', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'type', 'country', 'valute', 'period_job', 'prob_period', 'prob_price', '2024-08-28 15:37:32', '120', '100', '2024-08-28 15:37:32', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'img_main', 'img_banner', 'img_video_promo', 'video_promo')";
 //        VALUES ('name', 'detail', 'state', 'sell_all', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'type', 'country', 'valute', 'period_job', 'prob_period', 'prob_price',
 //        '2024-08-28 15:37:32', '120', '100', '2024-08-28 15:37:32', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'img_main', 'img_banner',
 //                            'img_video_promo', 'video_promo')";
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
+//if ($conn->query($sql) === TRUE) {
+  //echo "New record created successfully";
+//} else {
+  //echo "Error: " . $sql . "<br>" . $conn->error;
+//}
 
-$conn->close();
+//$conn->close();
+mysqli_close($conn);
 //echo "test";
