e2d573a36dc0dfc2a9879d940f240de712957cb2
unknown <diurtorn@gmail.com>
2024-08-28 17:13:12 +0300

    [*]

--- public/php/product_add.php
+++ public/php/product_add.php
@@ -1,3 +1,25 @@
 <?php
 
-echo "Test";
+$servername = "localhost";
+$username = "diur_admin";
+$password = "%v~kfAt5W#d82sNCWZiF";
+$dbname = "phoenix_fun";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+//VALUES ('John', 'Doe', 'john@example.com')";
+
+//if ($conn->query($sql) === TRUE) {
+  //echo "New record created successfully";
+//} else {
+  //echo "Error: " . $sql . "<br>" . $conn->error;
+//}
+
+$conn->close();
+echo "test";
