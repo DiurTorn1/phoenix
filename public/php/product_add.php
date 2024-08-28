437a6dd4ef77dd5fe690a49f8abf154191d7cbce
unknown <diurtorn@gmail.com>
2024-08-28 18:13:35 +0300

    [*]

--- public/php/product_add.php
+++ public/php/product_add.php
@@ -19,6 +19,20 @@ if (!$conn) {
 } else {
     echo "test";
 }
+
+// Prepare the INSERT query
+$query = "INSERT INTO `products`(`id`, `name`, `detail`, `datetime_start`, `datetime_stop`, `type`, `country`, `valute`, `period_job`, `prob_period`, `prob_price`, `datetime_start_sell`, `price`, `old_price`, `datetime_start_access`, `datetime_stop_access`, `datetime_stop_sell`, `img_main`, `img_banner`, `img_video_promo`, `video_promo`) VALUES ('name', 'detail', 'state', 'sell_all', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'type', 'country', 'valute', 'period_job', 'prob_period', 'prob_price', '2024-08-28 15:37:32', '120', '100', '2024-08-28 15:37:32', '2024-08-28 15:37:32', '2024-08-28 15:37:32', 'img_main', 'img_banner', 'img_video_promo', 'video_promo')";
+
+// Execute the query
+$result = mysqli_query($conn, $query);
+
+// Check for success or error
+if ($result) {
+    echo "Data inserted successfully!";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
 // Create connection
 //$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
