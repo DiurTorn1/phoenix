14b86ffb4277fb1daad207f66cb1733429e84bf1
unknown <diurtorn@gmail.com>
2024-08-30 14:13:56 +0300

    [+]add test code

--- public/js/Backbone.js
+++ public/js/Backbone.js
@@ -2153,3 +2153,22 @@
   
     return Backbone;
   });
+
+  $(document).ready(function() {
+    $("#exel_users_load").on('click', function(){
+        Backbone.TemplateManager.baseUrl = 'templates/{name}';
+        //var xml = '<rss version="2.0"><channel><title>RSS Title</title></channel></rss>',
+        //xmlDoc = $.parseXML( xml ),
+        //$xml = $( xmlDoc ),
+       // $title = $xml.find( 'title' );  
+        // вставим "RSS Title" в #someElement
+        //console.log( $title.text() );    
+ 
+        // поменяем его значение на "XML Title"
+       // $title.text( 'XML Title' );
+ 
+        // вставим "XML Title" уже в #anotherElement
+       // console.log( $title.text() );
+        
+    });
+});
