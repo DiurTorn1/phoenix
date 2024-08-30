94fe9e1fc5a10a5b39b2f41fa450f652506fe150
unknown <diurtorn@gmail.com>
2024-08-30 13:59:09 +0300

    [+]add test code

--- public/js/exel_user_load.js
+++ public/js/exel_user_load.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
     $("#exel_users_load").on('click', function(){
-        var xml = '<rss version="2.0"><channel><title>RSS Title</title></channel></rss>',
-        xmlDoc = $.parseXML( xml ),
-        $xml = $( xmlDoc ),
-        $title = $xml.find( 'title' );  
+        Backbone.TemplateManager.baseUrl = 'templates/{name}';
+        //var xml = '<rss version="2.0"><channel><title>RSS Title</title></channel></rss>',
+        //xmlDoc = $.parseXML( xml ),
+        //$xml = $( xmlDoc ),
+       // $title = $xml.find( 'title' );  
         // вставим "RSS Title" в #someElement
-        console.log( $title.text() );    
+        //console.log( $title.text() );    
  
         // поменяем его значение на "XML Title"
-        $title.text( 'XML Title' );
+       // $title.text( 'XML Title' );
  
         // вставим "XML Title" уже в #anotherElement
-        console.log( $title.text() );
+       // console.log( $title.text() );
         
     });
 });
