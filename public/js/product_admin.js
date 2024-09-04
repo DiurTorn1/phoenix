3930eb0c124b704831ca0d324e11fac86786cfc7
unknown <diurtorn@gmail.com>
2024-09-04 16:14:44 +0300

    [*]

--- public/js/product_admin.js
+++ public/js/product_admin.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     $("#delete_product").on('click', function() {
         var idarr = $("#main_parent_parents").find("li").map(function() { return this.id; }).get(); //ToArray
         for(var i = 0; i < idarr.length; i++){
-            var idparr = $("#"+idarr[i]).find("div").map(function() { return this.id; }).get(); //ToArray
+            var idstr = "#"+idarr[i];
+            var idparr = $(idstr).find("div").map(function() { return this.id; }).get(); //ToArray
             console.log(idparr);
         }
             
