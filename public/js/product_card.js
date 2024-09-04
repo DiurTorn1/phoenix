e4875257ba909138e0cc2e7f03fd4f88ab54e590
unknown <diurtorn@gmail.com>
2024-09-04 19:55:38 +0300

    [+]add test code

--- public/js/product_card.js
+++ public/js/product_card.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     var params = new window.URLSearchParams(window.location.search);
-    var pars = params.split("=");
+    var pars = params[0].split("=");
     var id = pars[1];
-    $.post('/php/get_product_card.php', {id:id}, function(data)  {
-        console.log(data);
-    });
+    console.log(id);
+    //$.post('/php/get_product_card.php', {id:id}, function(data)  {
+        //console.log(data);
+    //});
     //alert(params);
     
 });
