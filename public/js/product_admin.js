b83b9e7f72e26aae7de8b877747c79192758a5e2
unknown <diurtorn@gmail.com>
2024-09-04 16:29:35 +0300

    [*]

--- public/js/product_admin.js
+++ public/js/product_admin.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#delete_product").on('click', function() {
         var idarr = $("#main_parent_parents").find("li").map(function() { return this.id; }).get(); //ToArray
         for(var i = 0; i < idarr.length; i++){
-            var idparr = $('#' + idarr[i]).find("div.admin-video-sort-wrap");//.map(function() { return this.id; }).get(); //ToArray
+            var idparr = $('#' + idarr[i]);//.find("div.admin-video-sort-wrap");//.map(function() { return this.id; }).get(); //ToArray
             console.log(idparr);
         }
             
