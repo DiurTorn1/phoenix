e20cda22cde548b32a9d1fe9777d839199a8c640
unknown <diurtorn@gmail.com>
2024-09-04 20:19:52 +0300

    [+]add test code

--- public/php/get_product_card.php
+++ public/php/get_product_card.php
@@ -11,14 +11,14 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);//res_parse_stop_access
 }
 
-$sql = "SELECT * FROM `products` WHERE id=".$_POST['id'];
+$sql = "SELECT `id`, `name`, `detail`, `datetime_start`, `datetime_stop`, `type`, `country`, `valute`, `period_job`, `prob_period`, `prob_price`, `datetime_start_sell`, `price`, `old_price`, `datetime_start_access`, `datetime_stop_access`, `datetime_stop_sell`, `img_main`, `img_banner`, `img_video_promo`, `video_promo`, `created_at` FROM `products` WHERE id=".$_POST['id'];
 
-echo $conn->query($sql);
-//if ($conn->query($sql) === TRUE) {
-  //echo $conn->query($sql);
-//} else {
-  //echo "Error: " . $sql . "/" . $conn->error;
-//}
+//echo $conn->query($sql);
+if ($conn->query($sql) === TRUE) {
+  echo $conn->query($sql);
+} else {
+  echo "Error: " . $sql . "/" . $conn->error;
+}
 
 $conn->close();
     //echo "I get param1 = ".$_POST['id'];
