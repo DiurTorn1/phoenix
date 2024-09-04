1132acb5a38468d608d160d04ad8a584028a22b8
unknown <diurtorn@gmail.com>
2024-09-04 16:13:18 +0300

    [*]

--- public/js/product_admin.js
+++ public/js/product_admin.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
     $("#delete_product").on('click', function() {
-        var idarr = $("#main_parent_parents")
-        .find("li") //Find the spans
-        .map(function() { return this.id; }) //Project Ids
-        .get(); //ToArray
+        var idarr = $("#main_parent_parents").find("li").map(function() { return this.id; }).get(); //ToArray
         for(var i = 0; i < idarr.length; i++){
-            console.log(idarr[i]);
+            var idparr = $("#"+idarr[i]).find("div").map(function() { return this.id; }).get(); //ToArray
+            console.log(idparr);
         }
             
         
