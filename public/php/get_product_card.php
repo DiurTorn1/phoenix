3fc675a20a0613395d5f56aeb8b991adce94331c
unknown <diurtorn@gmail.com>
2024-09-05 18:27:53 +0300

    [*]

--- public/php/get_product_card.php
+++ public/php/get_product_card.php
@@ -5,50 +5,23 @@ $password = "%v~kfAt5W#d82sNCWZiF";
 $dbname = "phoenix_fun";
 
 // Create connection
-/*$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);//res_parse_stop_access
 }
 
-$sql = "DELETE FROM `products` WHERE id=" . $_POST['id'];
+$sql = "SELECT * FROM `products` WHERE id=28";//.$_POST['id'];
 
-
+$result = $mysqli->query($sql);
+$row = $result->fetch_row();
+//echo $conn->query($sql);
 //if ($conn->query($sql) === TRUE) {
- // echo "Delete product: " . $_POST['id'];
+  //echo $conn->query($sql);
 //} else {
   //echo "Error: " . $sql . "/" . $conn->error;
 //}
-$result=$conn->query($sql);
-  while($row = mysql_fetch_array($result)){
-    $id=$row['id'];
-    $name=$row['name'];
-    $detail=$row['detail'];
-   $datetime_start=$row['datetime_start'];
-    $datetime_stop=$row['datetime_stop'];
- 
-    echo "$id - $name - $detail - $datetime_start - $population";
-}
-$conn->close();*/
 
-$link = mysql_connect($servername, $username, $password, $dbname);
-if (!$link) {
-    die('Ошибка соединения: ' . mysql_error());
-}
-echo 'Успешно соединились';
-
- 
-//$result=mysql_query("SELECT * FROM `world`.`city` WHERE id=670");
-  //while($row = mysql_fetch_array($result)){
-    //$id=$row['ID'];
-    //$name=$row['Name'];
-    //$country_code=$row['CountryCode'];
-   //$district=$row['District'];
-    //$population=$row['Population'];
- 
-    //echo "<p>$id - $name - $country_code - $district - $population</p>";
-    //}
-mysql_close($link);
-
-//`id`, `name`, `detail`, `datetime_start`, `datetime_stop`, `type`, `country`, `valute`, `period_job`, `prob_period`, `prob_price`, `datetime_start_sell`, `price`, `old_price`, `datetime_start_access`, `datetime_stop_access`, `datetime_stop_sell`, `img_main`, `img_banner`, `img_video_promo`, `video_promo`, `created_at`
+$conn->close();
+    //echo "I get param1 = ".$_POST['id'];
 ?>
