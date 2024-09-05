c70b11aab759b4827611b34ef6fcdc49911ee18c
unknown <diurtorn@gmail.com>
2024-09-05 16:29:40 +0300

    [+]add test code

--- public/php/get_product_card.php
+++ public/php/get_product_card.php
@@ -11,14 +11,27 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);//res_parse_stop_access
 }
 
-$sql = "SELECT * FROM `products` WHERE id=28";//.$_POST['id'];
+//$sql = "SELECT * FROM `products` WHERE id=28";//.$_POST['id'];
 
-echo $conn->query($sql);
+//echo $conn->query($sql);
 //if ($conn->query($sql) === TRUE) {
   //echo $conn->query($sql);
 //} else {
   //echo "Error: " . $sql . "/" . $conn->error;
 //}
+
+// проверка и получение id из строки запроса
+if (!isset($_GET['id'])) {
+  die(http_response_code(404));
+}
+
+$id = $_GET['id']; 
+// проверка на число
+if (!ctype_digit($id)) {
+  die(http_response_code(400));
+}
+
+echo $_GET['id'];
 
 $conn->close();
     //echo "I get param1 = ".$_POST['id'];
