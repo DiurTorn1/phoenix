d9ae43ce8c5aba74d71dbc53a5b0f934407b705e
unknown <diurtorn@gmail.com>
2024-09-05 19:10:13 +0300

    [*]

--- public/php/get_product_card.php
+++ public/php/get_product_card.php
@@ -4,17 +4,8 @@ require "config_bd.php";
 $sql = "SELECT `id`, `name`, `detail`, `datetime_start`, `datetime_stop`, `type`, `country`, `valute`, `period_job`, `prob_period`, `prob_price`, `datetime_start_sell`, `price`, `old_price`, `datetime_start_access`, `datetime_stop_access`, `datetime_stop_sell`, `img_main`, `img_banner`, `img_video_promo`, `video_promo`, `created_at` FROM `products` WHERE id=".$_POST['id'];
 
 $result = $conn->query($sql);
-//$row = $result->fetch_row();
-//echo $row;
-while($row = $result->fetch_row()){
-    $id=$row['id'];
-    $name=$row['name'];
-    $detail=$row['detail'];
-    $datetime_start=$row['datetime_start'];
-    $datetime_stop=$row['datetime_stop'];
-
-  echo "<p>$id - $name - $detail - $datetime_start - $datetime_stop</p>";
-}
+$row = $result->fetch_row();
+echo $row[0] . "&" . $row[1] . "&" . $row[2] . "&" . $row[3] . "&" . $row[4] . "&" . $row[5];
 //if ($conn->query($sql) === TRUE) {
   //echo $conn->query($sql);
 //} else {
