449978774ca05121d21f0ea980ffb82d476db39b
unknown <diurtorn@gmail.com>
2024-09-05 19:00:47 +0300

    [*]

--- public/php/get_product_card.php
+++ public/php/get_product_card.php
@@ -4,8 +4,8 @@ require "config_bd.php";
 $sql = "SELECT `id`, `name`, `detail`, `datetime_start`, `datetime_stop`, `type`, `country`, `valute`, `period_job`, `prob_period`, `prob_price`, `datetime_start_sell`, `price`, `old_price`, `datetime_start_access`, `datetime_stop_access`, `datetime_stop_sell`, `img_main`, `img_banner`, `img_video_promo`, `video_promo`, `created_at` FROM `products` WHERE id=".$_POST['id'];
 
 $result = $conn->query($sql);
-//$row = $result->fetch_row();
-//echo $conn->query($sql);
+$row = $result->fetch_row();
+echo $row;
 //if ($conn->query($sql) === TRUE) {
   //echo $conn->query($sql);
 //} else {
