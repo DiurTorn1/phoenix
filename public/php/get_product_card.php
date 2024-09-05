b913324bd733fd85780633a9829c3c8391cc5392
unknown <diurtorn@gmail.com>
2024-09-05 18:31:30 +0300

    [*]

--- public/php/get_product_card.php
+++ public/php/get_product_card.php
@@ -11,10 +11,10 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);//res_parse_stop_access
 }
 
-$sql = "SELECT * FROM `products` WHERE id=28";//.$_POST['id'];
+$sql = "SELECT * FROM `products` WHERE id=".$_POST['id'];
 
 $result = $mysqli->query($sql);
-$row = $result->fetch_row();
+//$row = $result->fetch_row();
 //echo $conn->query($sql);
 //if ($conn->query($sql) === TRUE) {
   //echo $conn->query($sql);
