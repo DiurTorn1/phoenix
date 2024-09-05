0575b377ed5fd7172382194b37b042bba1f18d28
unknown <diurtorn@gmail.com>
2024-09-05 18:59:42 +0300

    [*]

--- public/php/get_product_card.php
+++ public/php/get_product_card.php
@@ -1,9 +1,9 @@
 <?php
 require "config_bd.php";
 
-//$sql = "SELECT * FROM products WHERE id=".$_POST['id'];
+$sql = "SELECT `id`, `name`, `detail`, `datetime_start`, `datetime_stop`, `type`, `country`, `valute`, `period_job`, `prob_period`, `prob_price`, `datetime_start_sell`, `price`, `old_price`, `datetime_start_access`, `datetime_stop_access`, `datetime_stop_sell`, `img_main`, `img_banner`, `img_video_promo`, `video_promo`, `created_at` FROM `products` WHERE id=".$_POST['id'];
 
-//$result = $mysqli->query($sql);
+$result = $conn->query($sql);
 //$row = $result->fetch_row();
 //echo $conn->query($sql);
 //if ($conn->query($sql) === TRUE) {
@@ -12,6 +12,6 @@ require "config_bd.php";
   //echo "Error: " . $sql . "/" . $conn->error;
 //}
 
-//$conn->close();
+$conn->close();
     //echo "I get param1 = ".$_POST['id'];
 ?>
