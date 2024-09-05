6157b832e0dcb0a5a35ca1e1dd40ae54e651c7a4
unknown <diurtorn@gmail.com>
2024-09-05 19:17:25 +0300

    [*]

--- public/js/product_card.js
+++ public/js/product_card.js
@@ -4,7 +4,31 @@ $(document).ready(function() {
     var id = params.get('product_id');
     //console.log(id);
     $.post('/php/get_product_card.php', {id:id}, function(data)  {
-        console.log(data);
+        //console.log(data);
+        var pars = data.split("&");
+        console.log(pars[0]);
+        console.log(pars[1]);
+        console.log(pars[2]);
+        console.log(pars[3]);
+        console.log(pars[4]);
+        console.log(pars[5]);
+        console.log(pars[6]);
+        console.log(pars[7]);
+        console.log(pars[8]);
+        console.log(pars[9]);
+        console.log(pars[10]);
+        console.log(pars[11]);
+        console.log(pars[12]);
+        console.log(pars[13]);
+        console.log(pars[14]);
+        console.log(pars[15]);
+        console.log(pars[16]);
+        console.log(pars[17]);
+        console.log(pars[18]);
+        console.log(pars[19]);
+        console.log(pars[20]);
+        console.log(pars[21]);
+
     });
     //alert(params);
     
