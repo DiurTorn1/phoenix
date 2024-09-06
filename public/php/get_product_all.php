4e1f1cba6490a4b023df7e5269a3a5a1b0a8834c
unknown <diurtorn@gmail.com>
2024-09-06 20:01:43 +0300

    [+]add test code

--- public/php/get_product_all.php
+++ public/php/get_product_all.php
@@ -4,7 +4,7 @@ require "config_bd.php";
 $sql = "SELECT `id`, `id_product`, `type`, `create_at`, `initial` FROM `product_public_permission` WHERE 1";
 
 $result = $conn->query($sql);
-$row = $result->fetch_row();
+$row = $result->fetch_array();
 echo var_dump($row);//[0] . "&" . $row[1] . "&" . $row[2] . "&" . $row[3];
 //if ($conn->query($sql) === TRUE) {
   //echo $conn->query($sql);
