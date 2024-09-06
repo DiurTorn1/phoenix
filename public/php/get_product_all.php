f21b9149b503d2d58e1f8d27b2f9f4ea39580659
unknown <diurtorn@gmail.com>
2024-09-06 20:04:20 +0300

    [+]add test code

--- public/php/get_product_all.php
+++ public/php/get_product_all.php
@@ -1,11 +1,15 @@
 <?php
 require "config_bd.php";
 
-$sql = "SELECT `id`, `id_product`, `type`, `create_at`, `initial` FROM `product_public_permission` WHERE 10000";
+$sql = "SELECT `id`, `id_product`, `type`, `create_at`, `initial` FROM `product_public_permission` WHERE 1";
 
 $result = $conn->query($sql);
-$row = $result->fetch_array();
-echo var_dump($row);//[0] . "&" . $row[1] . "&" . $row[2] . "&" . $row[3];
+$row = $result->fetch_row();
+while ($row = $result->fetch_array(MYSQLI_BOTH)) {
+    // Look inside $row here, do what you want with it.
+    echo var_dump($row);
+}
+//echo var_dump($row);//[0] . "&" . $row[1] . "&" . $row[2] . "&" . $row[3];
 //if ($conn->query($sql) === TRUE) {
   //echo $conn->query($sql);
 //} else {
