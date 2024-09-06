38c741187c532b45a2f99c612d5882ff82a23e84
unknown <diurtorn@gmail.com>
2024-09-06 20:16:21 +0300

    [+]add test code

--- public/js/get_cardid_stream.js
+++ public/js/get_cardid_stream.js
@@ -60,7 +60,10 @@ $(document).ready(function() {
                     $.post('/php/get_product_all.php', function(data)  {
                         //for(var i=0; i<data.length;i++){
                             var output = $.parseJSON(data);
-                            console.log(output);
+                            $.each(output,function(i,item){
+                                console.log(item.id_product);
+                            });
+                            
                         //}
                         
                     });
