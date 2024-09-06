5b362f9c06380c295a546e6b9eb4d408aa06df49
unknown <diurtorn@gmail.com>
2024-09-06 17:51:39 +0300

    [+]add test code

--- public/js/get_video_user.js
+++ public/js/get_video_user.js
@@ -39,10 +39,11 @@ $(document).ready(function() {
                                         '<div class="index-live-list">' +
                                             '<div class="index-live-item">' +
                                                     '<div class="index-live-item-video">' +
-                                                        '<a href="" tabindex="0">' +
+                                                        '<a href="#" tabindex="0">' +
                                                             '<span class="label">LIVE</span>' +
                                                         '</a>' +
                                                     '</div>' +
+                                                    '<button id="bay_ticket">Купить билет</button>' +
                                                     '<button id="bay_trainsport">Купить подписку</button>' +
                                                 '<div class="index-live-item-text">' +
                                                     '<a href="#">' + item.name + '</a>' +
