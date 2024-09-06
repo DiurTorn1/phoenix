e5cc79ecc3af1c05572d5b1640e4b52127e7765e
unknown <diurtorn@gmail.com>
2024-09-06 16:09:10 +0300

    [+]add test code

--- public/php/get_stream_public.php
+++ public/php/get_stream_public.php
@@ -1,7 +1,7 @@
 <?php
 require "config_bd.php";
 
-$sql = "SELECT `id`, `name_stream`, `type`, `create_at` FROM `stream_public_permission` WHERE name_stream=".$_POST['name_stream'];
+$sql = "SELECT `id`, `name_stream`, `type`, `create_at` FROM `stream_public_permission` WHERE name_stream='".$_POST['name_stream']."'";
 
 $result = $conn->query($sql);
 $row = $result->fetch_row();
@@ -9,7 +9,7 @@ echo $row[0] . "&" . $row[1] . "&" . $row[2] . "&" . $row[3];
 //if ($conn->query($sql) === TRUE) {
   //echo $conn->query($sql);
 //} else {
-  //echo "Error: " . $sql . "/" . $conn->error;
+  //echo "Error: " . $sql . "/" . $conn->error;    Horoshaya igra
 //}
 
 $conn->close();
