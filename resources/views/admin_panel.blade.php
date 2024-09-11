5bfb5c74bc5375596e2277c35de23152b73bf57d
unknown <diurtorn@gmail.com>
2024-09-11 18:38:26 +0300

    [*]

--- resources/views/admin_panel.blade.php
+++ resources/views/admin_panel.blade.php
@@ -70,7 +70,7 @@
 				<li class="admin-navigation-item"><a href="{{ route('users.index') }}" class="admin-navigation-link"><img class="admin-menu-icon" src="{{ asset('img/analitik.png') }}" alt="Пользователи">Пользователи</a></li>
 				<li class="admin-navigation-item"><a href="/tags" class="admin-navigation-link"><img class="admin-menu-icon" src="img/tag.png" alt="Теги">Теги</a></li>
 				<li class="admin-navigation-item"><a href="/access_in_countries" class="admin-navigation-link"><img class="admin-menu-icon" src="img/earch.png" alt="Доступ в странах">Доступ в странах</a></li>
-				<li class="admin-navigation-item"><a href="#" class="admin-navigation-link"><img class="admin-menu-icon" src="img/entry.png" alt="Entrypoints">Entrypoints</a></li>
+				<li class="admin-navigation-item"><a href="/entrypoint" class="admin-navigation-link"><img class="admin-menu-icon" src="img/entry.png" alt="Entrypoints">Entrypoints</a></li>
 				<li class="admin-navigation-item"><a href="#" class="admin-navigation-link"><img class="admin-menu-icon" src="img/analitik.png" alt="Виджеты">Виджеты</a></li>
 			</ul>
 		</nav>
