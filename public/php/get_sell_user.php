a92d14edc00a6f48b4712161c0327b9484149a3f
unknown <diurtorn@gmail.com>
2024-09-12 15:59:37 +0300

    [*]

--- public/php/get_sell_user.php
+++ public/php/get_sell_user.php
@@ -5,12 +5,14 @@ $sql = "SELECT `id`, `product_id`, `user_email`, `create_at`, `type_sell` FROM `
 
 $result = $conn->query($sql);
 $row = $result->fetch_row();
-echo $row[0] . "&" . $row[1] . "&" . $row[2] . "&" . $row[3] . "&" . $row[4];
+//echo $row[0] . "&" . $row[1] . "&" . $row[2] . "&" . $row[3] . "&" . $row[4];
 //if ($conn->query($sql) === TRUE) {
   //echo $conn->query($sql);
 //} else {
   //echo "Error: " . $sql . "/" . $conn->error;
 //}
+//$news_events = $result->fetch_all(MYSQLI_ASSOC);
+echo json_encode($row);
 
 $conn->close();
     //echo "I get param1 = ".$_POST['id'];
