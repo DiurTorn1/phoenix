b14f7192143b4e726f0df7b5003e06e343e9f989
unknown <diurtorn@gmail.com>
2024-09-13 12:06:00 +0300

    [*]

--- public/js/entrypoint.js
+++ public/js/entrypoint.js
@@ -4,46 +4,26 @@ $(document).ready(function(){
         var output = $.parseJSON(data);
         //console.log(output);
         $.each(output,function(i,item){
-            console.log(item.sity);
-            console.log(item.zal);
-            console.log(item.key_stream);
-            console.log(item.create_at);
+            //console.log(item.sity);
+            //console.log(item.zal);
+            //console.log(item.key_stream);
+            //console.log(item.create_at);
+            var pars = item.create_at.split(" ");
+            $("#entrypoints_view").append(
+                '<tr class="tr-wrapper admin-video-item admin-back">'+
+                    '<td class="table-one-column-check">' +
+                        '<div class="admin-video-sort-wrap">' +
+                            '<input id="check-promo-1" class="admin-video-sort-check" type="checkbox">' +
+                            '<label class="admin-video-check-label" for="check-promo-1"></label>' +
+                        '</div>' +
+                    '</td>' +
+                    '<th class="table-one-column-70">' + item.sity + '(' + item.zal + ')</th>' +
+                    '<td><p class="promo-prew-admin">Оffline</p></td>' +
+                    '<td>' + pars[0] + '</td>' +
+                '</tr>');
         });
     });
-    /*$("#admin-video-list").append(
-        '<li class="admin-video-item admin-back" id="' + item.id + '">' + 
-            '<div class="admin-video-sort-wrap">' + 
-                '<input id="admin-vl' + idch +'" class="admin-video-sort-check" type="checkbox">' +
-                '<label class="admin-video-check-label" for="admin-vl' + idch + '"></label>' + 
-            '</div>' + 	
-            '<div class="admin-video-prev">' + 
-                '<img src="' + item.poster.sm + '" alt="" class="" style="heigth: 100%; width: 100%">' +
-                '<img src="img/rub2.png" alt="" class="admin-video-prev-stik">' +
-                //'<p>Нет потока</p>' + 
-            '</div>' + 
-            '<div class="admin-video-item-header">' + 
-                '<div class="admin-video-item-h-list">' + 
-                '<span class="admin-video-item-h-date">' + item.name + '</span>' +
-            '</div>' + 
-            '<div class="admin-video-item-h-hashtag">' + 
-                '<span class="admin-video-hashtag-date">' + sezon + '</span>' + 
-                '<span class="admin-video-hashtag-kubok">' + kubok + '</span>' +
-                '<span class="admin-video-hashtag-group">' + weigth + '</span>' +
-                '<span class="admin-video-hashtag-sport">'+ vid_sport +'</span>' +
-                '<span class="admin-video-hashtag-city">' + gorod + '</span>' +
-                '<span class="admin-video-hashtag-org">' + boss + '</span>' +
-            '</div>' +
-        '</div>' +
-        '<div class="admin-video-item-view">' +
-            '<p class="admin-video-item-v-count"><span></span>просмотра</p>' +
-            '<p class="admin-video-item-v-region">' + region + '</p>' +
-        '</div>' +
-        '<div class="admin-video-item-info admin-video-item-view">' +
-            '<p class="admin-video-item-i-time">Закончилась в<span></span></p>' +
-            '<p class="admin-video-item-i-date">' + item.stream.finished_at + '</p>' +
-            '<a href="#" class="admin-video-item-i-link"><img src="img/right-arrow.svg" alt="Поделиться"></a>' +
-        '</div>' + 
-    '</li>');*/
+    /**/
     // Открыть/Закрыть ОКНО СОЗДАТЬ ENTRY
     $("#add-new-entrypoint").click(function(){
         $("#open-create-entrypoint").addClass('add-price-show'); 
