7b58d178707cfcb921ec0216e294b7aa4da41ede
unknown <diurtorn@gmail.com>
2024-09-13 11:36:55 +0300

    [*]

--- public/php/entrypoints_add.php
+++ public/php/entrypoints_add.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);//res_parse_stop_access
 }
 
-$sql = "INSERT INTO `entrypoints`(`sity`, `zal`, `key_stream`, `create_at`) VALUES ('". $_POST['sity_entrypoints'] ."','". $_POST['zal_entrypoints'] ."','". $_POST['key_entrypoints'] ."','". $_POST['create_at'] ."')";
+$sql = "INSERT INTO `entrypoints`(`sity`, `zal`, `key_stream`, `create_at`) VALUES ('". $_POST['sity_entrypoints'] ."','". $_POST['zal_entrypoints'] ."','". $_POST['key_entrypoints'] ."','2024-09-13 11:36:32')";
 
 if ($conn->query($sql) === TRUE) {
   echo "OK";
