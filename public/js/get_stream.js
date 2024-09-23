04e1fe4b8d7e79d7606113a8fdec74a0a9c0b9b4
unknown <diurtorn@gmail.com>
2024-09-23 18:44:59 +0300

    [*]

--- public/js/get_stream.js
+++ public/js/get_stream.js
@@ -86,10 +86,9 @@ $(document).ready(function() {
                                 key_post = 1;
                             }
                         });
-                        console.log("get_product_gl: " + key_post);
                         
                     });   
-                
+                console.log("get_product_gl: " + key_post);
                 //product_global = '<img src="img/rub2.png" alt="" class="admin-video-prev-stik">';
                 $("#admin-video-list").append(
                     '<li class="admin-video-item admin-back" id="' + item.id + '">' + 
