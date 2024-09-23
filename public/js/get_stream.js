0d719a2f89327c57169564f81bb6c88c2c86af01
unknown <diurtorn@gmail.com>
2024-09-23 17:56:08 +0300

    [*]

--- public/js/get_stream.js
+++ public/js/get_stream.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     //$("#admin-video-list-btn").on('click', function(){
         var main = $('#admin-input-main').val();
         var idch = 0, key_post = 0;
+        var get_product_gl = '', initial_gl;
         $.post('/php/get_stream.php', function(data)  {
             //alert("Text:"+data);
             //console.log(data);
@@ -74,7 +75,7 @@ $(document).ready(function() {
                     //console.log(res_parstart1[2] + " " + res_parstart1[1] + " " + res_parstart1[0]);
                     data_put = res_parstart1[2] + " " + res_parstart1[1] + " " + res_parstart1[0];
                 }
-                var get_product_gl = '', initial_gl;
+                
                 initial_gl = item.name;
                     $.post('/php/get_product_all.php', function(data)  {
                         var output1 = $.parseJSON(data);
@@ -83,6 +84,7 @@ $(document).ready(function() {
                             if(item1.initial === initial_gl){
                                 //get_product_gl = '<img src="img/rub2.png" alt="" class="admin-video-prev-stik">';
                                 key_post = 1;
+                                console.log("get_product_gl: " + key_post);
                             }
                         });
                         
@@ -124,7 +126,7 @@ $(document).ready(function() {
                             '</div>' + 
                         '</li>');
  
-                        console.log("get_product_gl: " + key_post);
+                        
 
                     
                 idch++;
