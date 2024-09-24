ab260154349e5c045f78a12499b63e47ea23942b
unknown <diurtorn@gmail.com>
2024-09-24 20:00:04 +0300

    [*]

--- public/js/get_stream.js
+++ public/js/get_stream.js
@@ -15,7 +15,7 @@ function dynamic_checkbox(){
 }
 
 $(document).ready(function() {
-    $('#admin-video-sort-check').val('Выбрано ' + 0 + ' стримов');
+    $('.admin-video-check-label').val('Выбрано ' + 0 + ' стримов');
     //dynamic_checkbox();
     //setInterval('dynamic_checkbox()',200);
     //$("#admin-video-list-btn").on('click', function(){
