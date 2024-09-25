2899371ea7f7461f040b836b70a6647b28195924
unknown <diurtorn@gmail.com>
2024-09-25 16:56:48 +0300

    [*]

--- public/js/get_stream.js
+++ public/js/get_stream.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
     setInterval('dynamic_checkbox()',200);
     get_stream_array();
     console.log(array_stream);
-    console.log(array_stream.size());
+    console.log(array_stream.length);
     
     //$("#admin-video-list-btn").on('click', function(){
         var main = $('#admin-input-main').val();
