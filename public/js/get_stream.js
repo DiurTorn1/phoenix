b5893ce29fdbba3f72144e0d4538f659471446fe
unknown <diurtorn@gmail.com>
2024-09-25 14:29:15 +0300

    [*]

--- public/js/get_stream.js
+++ public/js/get_stream.js
@@ -19,10 +19,22 @@ function dynamic_checkbox(){
     }
 }
 
+function get_stream_array(){
+    var array_stream = new Array();
+    var arr = [];
+    for(var i = 0; i < 20; i++){
+        arr = [i, 'push arr'];
+        array_stream.push(arr);
+    }
+    return array_stream;
+}
+
 $(document).ready(function() {
     
     dynamic_checkbox();
     setInterval('dynamic_checkbox()',200);
+    var arr_stream = get_stream_array();
+    console.log(arr_stream);
     //$("#admin-video-list-btn").on('click', function(){
         var main = $('#admin-input-main').val();
         
