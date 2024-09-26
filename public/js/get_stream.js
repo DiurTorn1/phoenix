0de615036b7e810f5439fb5dccedc078ce9cef6c
unknown <diurtorn@gmail.com>
2024-09-26 20:09:43 +0300

    [*]

--- public/js/get_stream.js
+++ public/js/get_stream.js
@@ -33,12 +33,12 @@ function paint_element_stream(){
         $.post('/php/get_stream_id.php',{ id:id_stream }, function(data)  {
             //console.log(data);
             var output = $.parseJSON(data);
-            //console.log(output);
             var list = output.data;
-            $.each(list,function(i,item){
+            console.log(list);
+            //$.each(list,function(i,item){
                 //console.log(item);
-                console.log("Video inform:\r\n");
-                console.log("id: " + item.id);// + "\r\nworkspace_id: " + item.workspace_id + "\r\nparent_id: " + item.parent_id + "\r\nname: " + item.name + "\r\nsubtitle: " + item.subtitle +
+                //console.log("Video inform:\r\n");
+                //console.log("id: " + item.id);// + "\r\nworkspace_id: " + item.workspace_id + "\r\nparent_id: " + item.parent_id + "\r\nname: " + item.name + "\r\nsubtitle: " + item.subtitle +
                     //"\r\ntype: " + item.type + "\r\nstreamkey: " + item.streamkey + "\r\nauto_start: " + item.auto_start + "\r\nprotected: " + item.protected + "\r\ntime_shift: " + item.time_shift); 
                 //console.log("\r\nRecord: \r\n parent_id: " + item.record.parent_id + "\r\nvideo: \r\n presets: " + item.video.presets + "\r\naudio: \r\n channel_mapping: " + item.audio.channel_mapping + 
                     //"\r\nreconnect_window:" + item.reconnect_window + "\r\nplay_link: " + item.play_link + "\r\nrtmp_link: " + item.rtmp_link + "\r\nscheduled: \r\n time: " + item.scheduled.time);
@@ -147,8 +147,8 @@ function paint_element_stream(){
                         
 
                     
-                idch++;
-            });
+                //idch++;
+            //});
         });
     }
 
