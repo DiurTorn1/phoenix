8e4dec5772351ab8d832f5c8269f234bb2be3ccb
unknown <diurtorn@gmail.com>
2024-09-28 18:04:52 +0300

    [*]

--- resources/views/card_product.blade.php
+++ resources/views/card_product.blade.php
@@ -114,13 +114,13 @@
 			<div class="admin-input-width50">
 				
 					<!--<a class="preview-product-vis-upload" id="upload_main_banner_product"> <p>Обложка продукта *</p>-->
-					<div class="index-live-banner">
-								<img src="img/no-image.jpg" alt="Обложка продукта *" id="upload_main_banner_product">
-					</div>
+					<img src="{{ asset('img/ivs4.png') }}" alt="Обложка продукта *" id="upload_main_banner_product">
 					<div class="edit-preview-tournir" id="banners_turnir">
 						<a class="remove-preview-tournir" title="Удалить обложку" id="img_banner_trash"><img src="{{ asset('img/trash.png') }}" alt="Удалить обложку"></a>
 					</div>
-					<!--<a href="" class="add-preview-tournir" title="Загрузить обложку продукта"><img src="{{ asset('img/ivs4.png') }}" alt="Обложка продукта *" id="upload_main_banner_product"></a> </a>-->
+					<!--<div class="index-live-banner">
+								<img src="img/no-image.jpg">
+					</div> </a>-->
 				
 			</div>
 			<div class="admin-input-width50">
