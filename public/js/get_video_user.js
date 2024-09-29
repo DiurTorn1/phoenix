1304b931c47e9e4c7ea2a123c7b4865bda0c8cfd
unknown <diurtorn@gmail.com>
2024-09-30 00:40:43 +0300

    [*]

--- public/js/get_video_user.js
+++ public/js/get_video_user.js
@@ -19,7 +19,7 @@ function user_sells(){
     
     $.post('/php/get_sell_user.php', {user_email:user_email}, function(data)  {
         var output = $.parseJSON(data);
-        console.log(output);
+        //console.log(output);
         var user_sell = output ? output[2]:''; 
         var user_product = output ? output[1]:'';
         if(user_sell){
