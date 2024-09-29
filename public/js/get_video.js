100e92b355543f021849b8878baa181b3c197887
unknown <diurtorn@gmail.com>
2024-09-29 11:38:35 +0300

    [*]

--- public/js/get_video.js
+++ public/js/get_video.js
@@ -1,30 +1,81 @@
+var array_product = new Array();
+var array_stream = new Array();
+var idch = 0;
+var key_main_chack = 0;
+var public_stream_list = new Array();
+var get_ch_list = 1;
+var key_paint = 0;
+
+function get_stream_array(){
+    
+    var count = 0;
+    $.post('/php/get_stream.php', function(data)  {
+        var output = $.parseJSON(data);
+        var pre_arr = new Array();
+        $.each(output.data,function(i,item){
+            pre_arr.push(item.id);
+            count++;
+        });
+        //pre_arr.push(output.data);
+        
+        for(var i = 0; i < pre_arr.length; i++){
+            array_stream.push(pre_arr[i]);
+            
+        }
+        count_stream = count;
+    });
+    if(!key_paint){
+        for(var i = 0; i < count_stream; i++){
+            array_stream1.push(array_stream[i]);           
+        }
+        if(array_stream1[0]){
+            paint_element_stream();
+            key_paint = 1;
+        }
+
+    }
+    //for(var i = 0; i < count_stream; i++){
+    if(array_stream[0]!=array_stream1[0]){
+            //setInterval('paint_element_stream()',100);
+        //$("#admin-video-list").empty();
+        //idch = 0;
+        key_paint = 0;
+    }
+    //}
+    //console.log(count_stream);
+    //console.log(array_stream);
+    //var list = json_product.data;
+}
+
 $(document).ready(function() {
+    get_stream_array();
+    setInterval('get_stream_array()',500);
     //$("#admin-video-list-btn").on('click', function(){
-        var main = $('#admin-input-main').val();
-        $.post('/php/get_stream.php', function(data)  {
+        //var main = $('#admin-input-main').val();
+        //$.post('/php/get_stream.php', function(data)  {
             //alert("Text:"+data);
             //console.log(data);
-            var output = $.parseJSON(data);
-            var list = output.data;
+           // var output = $.parseJSON(data);
+           // var list = output.data;
 
-            $.each(list,function(i,item){
-                console.log("Video inform:\r\n");
-                console.log(/*"id: " + item.id + "\r\nworkspace_id: " + item.workspace_id + "\r\nparent_id: " + item.parent_id + */"\r\nname: " + item.name /*+ "\r\nsubtitle: " + item.subtitle +
-                    "\r\ntype: " + item.type + "\r\nstreamkey: " + item.streamkey + "\r\nauto_start: " + item.auto_start + "\r\nprotected: " + item.protected + "\r\ntime_shift: " + item.time_shift*/); 
-                console.log(/*"\r\nRecord: \r\n parent_id: " + item.record.parent_id + "\r\nvideo: \r\n presets: " + item.video.presets + "\r\naudio: \r\n channel_mapping: " + item.audio.channel_mapping + 
-                    "\r\nreconnect_window:" + item.reconnect_window + */"\r\nplay_link: " + item.play_link /*+ "\r\nrtmp_link: " + item.rtmp_link + "\r\nscheduled: \r\n time: " + item.scheduled.time);
-                console.log("\r\nstream: \r\n id: " + item.stream.id + "\r\nevent_id: " + item.stream.event_id + "\r\nstatus: " + item.stream.status + "\r\nstarted_at: " + item.stream.started_at +  "\r\nfinished_at: " + item.stream.finished_at*/);
+            //$.each(list,function(i,item){
+                //console.log("Video inform:\r\n");
+                //console.log(/*"id: " + item.id + "\r\nworkspace_id: " + item.workspace_id + "\r\nparent_id: " + item.parent_id + */"\r\nname: " + item.name /*+ "\r\nsubtitle: " + item.subtitle +
+                    //"\r\ntype: " + item.type + "\r\nstreamkey: " + item.streamkey + "\r\nauto_start: " + item.auto_start + "\r\nprotected: " + item.protected + "\r\ntime_shift: " + item.time_shift*/); 
+                //console.log(/*"\r\nRecord: \r\n parent_id: " + item.record.parent_id + "\r\nvideo: \r\n presets: " + item.video.presets + "\r\naudio: \r\n channel_mapping: " + item.audio.channel_mapping + 
+                    //"\r\nreconnect_window:" + item.reconnect_window + */"\r\nplay_link: " + item.play_link /*+ "\r\nrtmp_link: " + item.rtmp_link + "\r\nscheduled: \r\n time: " + item.scheduled.time);
+                //console.log("\r\nstream: \r\n id: " + item.stream.id + "\r\nevent_id: " + item.stream.event_id + "\r\nstatus: " + item.stream.status + "\r\nstarted_at: " + item.stream.started_at +  "\r\nfinished_at: " + item.stream.finished_at*/);
                 //console.log("\r\nchat_after_stream: " + item.chat_after_stream + "\r\nchat_active: " + item.chat_active + "\r\nchat_preview: " + item.chat_preview + "\r\nshow_members: " + item.show_members +
                     //"\r\ncreated_at" + item.created_at + "\r\nupdated_at: " + item.updated_at + "\r\nlatency_mode: " + item.latency_mode + "\r\nallow_chat_links: " + item.allow_chat_links +
                     //"\r\nmoderators: " + item.moderators); '<a >'+ '<span class="label">LIVE</span>'+ '</a>' +
                 //console.log("\r\nposter \r\nid:" + item.poster.id + "\r\ntype: " + item.poster.type + "\r\nstatus" + item.poster.status + "\r\nactive: " + item.poster.active + "\r\noriginal: " + 
                     //"\r\nmd: " + item.poster.md + "\r\nsm: " + item.poster.sm + "\r\nxs: " + item.poster.xs +"\r\nfrom_time" + item.poster.from_time + "\r\nto_time" + item.poster.to_time);
-                    $("#slider").append(
+                    /*$("#slider").append(
                         '<div class="slide index-live-item" id="' + item.id + '">'+
                             '<div class="index-live-item-video">'+
                                 '<a >'+ 
                                     '<div class="index-live-banner">' +
-                                        '<img src="img/phoenix.png">' +
+                                        '<img src="' + item.poster.original + '">' +
                                     '</div>' +
                                 '</a>' +
                                 
@@ -35,7 +86,7 @@ $(document).ready(function() {
                         '</div>');
             });
             
-        });
+        });*/
 
         $(document).on('click', '.slide', function() {
             var id = this.id;
