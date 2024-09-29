b4a81ea3dd08e09a8f8f76f7f2791ecdea70d186
unknown <diurtorn@gmail.com>
2024-09-29 16:43:45 +0300

    [*]

--- public/js/get_video_user.js
+++ public/js/get_video_user.js
@@ -13,16 +13,17 @@ var user_global = "";
 function paint_element_stream(){
     var user_email = $('#name_user_get').text();
     //console.log(user_email);
-    var name_stream_gl = '';
+    var name_stream_gl = '', poster_stream_gl = '',id_stream_gl = '';
     for(var i = 0; i < count_stream; i ++){
+        var key_public = 0;
         $.post('/php/get_stream_id.php',{ id:array_stream[i] }, function(data)  {
             //console.log(data);
             var output = $.parseJSON(data);
             var list = output.data;
-            var key_public = 0;
-            name_stream_gl = list.name;
             
-            
+            name_stream_gl = list.name; 
+            poster_stream_gl = list.poster.original;
+            id_stream_gl = list.id;          
             //console.log("Video inform:\r\n");
             //console.log("id: " + list.id + "\r\nworkspace_id: " + list.workspace_id + "\r\nparent_id: " + list.parent_id + "\r\nname: " + list.name + "\r\nsubtitle: " + list.subtitle +
                 //"\r\ntype: " + list.type + "\r\nstreamkey: " + list.streamkey + "\r\nauto_start: " + list.auto_start + "\r\nprotected: " + list.protected + "\r\ntime_shift: " + list.time_shift); 
@@ -34,41 +35,37 @@ function paint_element_stream(){
                 //"\r\nmoderators: " + list.moderators);
             //console.log("\r\nposter \r\nid:" + list.poster.id + "\r\ntype: " + list.poster.type + "\r\nstatus" + list.poster.status + "\r\nactive: " + list.poster.active + "\r\noriginal: " + list.poster.original +
                 //"\r\nmd: " + list.poster.md + "\r\nsm: " + list.poster.sm + "\r\nxs: " + list.poster.xs +"\r\nfrom_time" + list.poster.from_time + "\r\nto_time" + list.poster.to_time);
-                $.post('/php/get_stream_public.php', {name_stream:list.name}, function(data)  {
-                    var output = $.parseJSON(data);
-                    //$.each(output,function(i,item){
-                    var key_public_get = 0;
-                    var pub_name = output? output[1]: '1';
-                        if(pub_name==name_stream_gl){
-                            console.log(pub_name);
-                            key_public_get = 1;
-                         }
-                         //console.log(output[2]);
-                         //console.log(output[3]);
-                         key_public = key_public_get;
-                         console.log("key_public: "+key_public);
-                   //});
-            
-                });
-                
-                if(key_public){
-                    $("#slider").append(
-                        '<div class="slide index-live-item" id="' + list.id + '">'+
-                            '<div class="index-live-item-video">'+
-                                '<a >'+ 
-                                    '<div class="index-live-banner">' +
-                                        '<img src="' + list.poster.original + '">' +
-                                    '</div>' +
-                                '</a>' +
-                                
-                            '</div>'+
-                            '<div class="index-live-item-text">'+
-                                '<a >' + list.name + '</a>'+
-                            '</div>'+
-                        '</div>');
-                }
 
         });
+        $.post('/php/get_stream_public.php', {name_stream:name_stream_gl}, function(data)  {
+            var output = $.parseJSON(data);
+            var key_public_get = 0;
+            var pub_name = output? output[1]: '1';
+            if(pub_name==name_stream_gl){
+                console.log(pub_name);
+                key_public_get = 1;
+            }
+            key_public = key_public_get;
+            console.log("key_public: "+key_public);
+    
+        });
+        
+        if(key_public){
+            $("#slider").append(
+                '<div class="slide index-live-item" id="' + id_stream_gl + '">'+
+                    '<div class="index-live-item-video">'+
+                        '<a >'+ 
+                            '<div class="index-live-banner">' +
+                                '<img src="' + poster_stream_gl + '">' +
+                            '</div>' +
+                        '</a>' +
+                        
+                    '</div>'+
+                    '<div class="index-live-item-text">'+
+                        '<a >' + name_stream_gl + '</a>'+
+                    '</div>'+
+                '</div>');
+        }
     }
 
 }
