dfd2f09d982254941be95f3904e5def5f22c5ffb
unknown <diurtorn@gmail.com>
2024-10-06 12:59:37 +0300

    [*]

--- public/js/get_cardid_stream.js
+++ public/js/get_cardid_stream.js
@@ -49,7 +49,8 @@ $(document).ready(function() {
                     parent_id_card = list.parent_id; 
                     play_link_card = list.play_link;
                     rtmp_link_card = list.rtmp_link;
-                    $('#card_link_play').val(list.play_link);
+                    //https://phoenix.tehnodir.ru/player_stream?player_stream_id=81cd4861-bccc-4d10-bad9-9296e9484652
+                    $('#card_link_play').val('https://phoenix.tehnodir.ru/player_stream?player_stream_id='+item.id);//list.play_link);
                     $('#img_poster_card').attr("src", list.poster.original);
                     $('#autoplay_video_ch').attr('checked', list.auto_start);
                     $('#chat_video_ch').attr('checked', list.chat_active);
