edb74b8e1c3f072f6e8b632c2768c24c887284ef
unknown <diurtorn@gmail.com>
2024-10-06 12:42:08 +0300

    [*]

--- public/js/get_cardid_stream.js
+++ public/js/get_cardid_stream.js
@@ -447,7 +447,7 @@ $(document).ready(function() {
     $("#copy_serch_link").on('click', function(){
         var $temp = $('#card_link_play').val();
         navigator.clipboard.writeText($temp);
-
+        $('.admin-video-link-copy').hide();
     });
 
     $("#card_stream_link").on('click', function(){
