fadee32451bd8f21504e0cb554ce497320522e80
unknown <diurtorn@gmail.com>
2024-10-09 00:23:00 +0300

    [*]

--- public/php/get_product_table.php
+++ public/php/get_product_table.php
@@ -1,23 +1,17 @@
 <?php
 require "config_bd.php";
 
-//$sql = "SELECT 'id', 'id_stream', 'reg_date' FROM ".$_POST['table']." WHERE id_stream='".$_POST['id_stream']."'";
+$sql = "SELECT 'id', 'id_stream', 'reg_date' FROM 'ticket_49' WHERE id_stream='".$_POST['id_stream']."'";
 
-//$result = $conn->query($sql);
-//$row = $result->fetch_row();
-//echo json_encode($row);// $row[0] . "&" . $row[1] . "&" . $row[2] . "&" . $row[3] . "&" . $row[4];
+$result = $conn->query($sql);
+$row = $result->fetch_row();
+echo json_encode($row);// $row[0] . "&" . $row[1] . "&" . $row[2] . "&" . $row[3] . "&" . $row[4];
 //if ($conn->query($sql) === TRUE) {
   //echo $conn->query($sql);
 //} else {
   //echo "Error: " . $sql . "/" . $conn->error;
 //}
 
-//$conn->close();
-    //echo "I get param1 = ".$_POST['id'];
-$sql = "SELECT 'id', 'id_stream', 'reg_date' FROM ".$_POST['table']." WHERE 1000";
-
-$result = $conn->query($sql);
-$news_events = $result->fetch_all(MYSQLI_ASSOC);
-echo json_encode($news_events);//[0] . "&" . $row[1] . "&" . $row[2] . "&" . $row[3];
 $conn->close();
+    //echo "I get param1 = ".$_POST['id'];
 ?>
