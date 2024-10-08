3de02bf1f52823304f3e4a76fb47cefdc047b80e
unknown <diurtorn@gmail.com>
2024-10-09 00:43:08 +0300

    [*]

--- public/js/get_cardid_stream.js
+++ public/js/get_cardid_stream.js
@@ -87,6 +87,7 @@ $(document).ready(function() {
                                                 key_product_get1 = 1;
                                                 console.log(item1.id_stream);
                                             }
+                                            console.log(key_product_get1);
                                         });
                                         key_product_get = key_product_get1;
                                     });
