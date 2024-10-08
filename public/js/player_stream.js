d278d09f825a3356051fa56c1cd915e10db7e069
unknown <diurtorn@gmail.com>
2024-10-08 14:17:15 +0300

    [*]

--- public/js/player_stream.js
+++ public/js/player_stream.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
                     //rtmp_link_card = item.rtmp_link;
 
                     $("#player_stream_get").append(
-                        '<div class="index-live-item-video">' +
+                        '<div class="index-live-item-video"style="width="100%"; height="100%";">' +
                             '<iframe src="' + item.play_link + '" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; encrypted-media; gyroscope; accelerometer; clipboard-write;"></iframe>' +
                         '</div>' +
                         '<div class="index-live-item-text">' +
