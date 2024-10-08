9ac7e7ccefd97b3857dfb752129d06c4c9b58c89
unknown <diurtorn@gmail.com>
2024-10-08 18:02:58 +0300

    [*]

--- public/php/create_table_product.php
+++ public/php/create_table_product.php
@@ -15,7 +15,7 @@ require "config_bd.php";
 //$conn->close();
     //echo "I get param1 = ".$_POST['id'];
     // sql to create table
-$sql = "CREATE TABLE '.$_POST['name'].' (
+$sql = "CREATE TABLE ".$_POST['name']." (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     id_stream VARCHAR(255) NOT NULL,
     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
