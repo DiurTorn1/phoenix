76057ed1509316dcf83d7bc68b3092739cfc78ad
unknown <diurtorn@gmail.com>
2024-10-11 01:57:44 +0300

    [*]

--- public/js/create_stream.js
+++ public/js/create_stream.js
@@ -1,4 +1,116 @@
 $(document).ready(function() {
+    var main_image;
+    var image, image_name = '';
+
+    $('#img_banner_trash').hide();
+
+
+    var r = new Resumable({
+        target: '/'
+      });
+      
+      
+      r.assignBrowse(document.getElementById('img_poster_card'));
+      
+      r.on('fileSuccess', function(file){
+            console.log('fileSuccess',file);
+            console.log(file.file);
+            //$('#img_poster_card').attr("src", file.file);
+            var reader = new FileReader();
+            
+            image_name = file.file.name;
+            reader.readAsDataURL(file.file);
+            //var output = $.parseJSON(file);
+            // var form_data = new FormData();
+            reader.onloadend = function(e) { 
+                //console.log(e.target.result);
+                $('#img_poster_card').attr("src", e.target.result);
+                //console.log(e.target.result);
+                main_image = e.target.result;
+                var image_res = e.target.result;
+                //console.log(image_res.split(',')[1]);
+                image = image_res.split(',')[1];
+                $.post('/php/upload_banners.php', { image: image, image_name:image_name }, function(data)  {
+                    //console.log(data);
+                    if(data === 'Successfully Uploaded'){ 
+                        //<!--<a href="#" class="remove-preview-tournir" title="Удалить обложку"><img src="{{ asset('img/trash.png') }}" alt="Удалить обложку"></a>-->
+                        //$("#banners_turnir").append('<a href="#" class="remove-preview-tournir" title="Удалить обложку"><img src="img/trash.png" alt="Удалить обложку"></a>');
+                        $('#img_banner_trash').show();
+                    }
+                });
+
+                //form_data.append('file',image);
+                /*$.ajax({
+                    url:'/php/upload_banners.php',
+                    method:'POST',
+                    data:form_data,
+                    contentType:false,
+                    processData:false,
+                    beforeSend:function(){
+                      //$('#msg').html('Loading......');
+                      console.log('Loading......');
+                    },
+                    success:function(data){
+                        //var output = $.parseJSON(data);
+                        console.log(data);
+                      //$('#msg').html(data);
+                    },
+                    error: function(xhr, ajaxOptions, thrownError) {
+                       console.log(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+                    }
+                });*/
+            };
+
+          
+        });
+      r.on('fileProgress', function(file){
+          console.log('fileProgress', file);
+        });
+      r.on('fileAdded', function(file, event){
+          r.upload();
+          console.log('fileAdded...');
+          //console.log('fileAdded...', event);
+          //var output = $.parseJSON(file);
+          //console.log(output);
+          //console.log(file);
+        });
+      r.on('filesAdded', function(array){
+          r.upload();
+          //console.log('filesAdded', array);
+        });
+      r.on('fileRetry', function(file){
+          console.log('fileRetry', file);
+        });
+      r.on('fileError', function(file, message){
+          console.log('fileError', file, message);
+        });
+      r.on('uploadStart', function(){
+          console.timeLog('uploadStart');
+        });
+      r.on('complete', function(){
+          console.log('complete');
+        });
+      r.on('progress', function(){
+          console.log('progress');
+        });
+      r.on('error', function(message, file){
+          console.log('error', message, file);
+        });
+      r.on('pause', function(){
+          console.log('pause');
+        });
+      r.on('cancel', function(){
+          console.log('cancel');
+        });
+
+    $("#img_banner_trash").on('click', function(){
+        $.post('/php/delete_banners.php', { image_name:image_name }, function(data)  {
+            if(data === 'Successfully delete'){
+                alert('Баннер удалён');
+                $('#img_banner_trash').hide();
+            }
+        });
+    });
     $("#save-st").on('click', function(){
         var main = $('#block-admin-inputct').val();
         var region = $("#admin_region_selectct option:selected").text();
@@ -41,6 +153,19 @@ $(document).ready(function() {
              $('#admin_link_playct').val(list.play_link);
              if(list.play_link){
                 alert("Стрим создан");
+                if(image_name){
+                    $.post('/php/upload_poster.php',{ image_name:image_name, id:list.id }, function(data){
+                        //console.log(data);
+                        var output = $.parseJSON(data);
+                        var list2 = output.data;
+                        $.each(list2,function(i,item2){
+                            if(item2.id == params.get('admin_input_id')){
+                                alert("Постер добавлен к стриму");
+                            }
+                        });
+    
+                    });
+                }
              } else {
                 alert("Ошибка создания стрима");
              }
