95c85ff0ecd6242cfb98835ea2b1ee8b9f330d4d
unknown <diurtorn@gmail.com>
2024-10-28 23:33:21 +0300

    [*]

--- public/js/get_video_nologin.js
+++ public/js/get_video_nologin.js
@@ -36,18 +36,22 @@ function paint_element_stream(){
             var id_stream_prod = list.id;
             var key_prod_perm_stream = 0;
             for(var i = 0; i < count_product; i ++){
+                var key_prod_perm_stream1 = 0;
                 $.post('/php/get_product_table.php',{ table:public_product_perm[i] }, function(data1)  {
-                    key_prod_perm_stream = 0;
+                    key_prod_perm_stream1 = 0;
                     var output1 = $.parseJSON(data1);
                     $.each(output1,function(i,item1){
                         if(item1.id_stream == id_stream_prod){
                             //console.log(item1.id_stream);
-                            key_prod_perm_stream = 1;
+                            key_prod_perm_stream1 = 1;
                         }
                         //console.log(id_stream_prod);
                     });
-                    console.log(key_prod_perm_stream);
+                    //console.log(key_prod_perm_stream);
                 });
+                if(key_prod_perm_stream1){
+                    console.log(key_prod_perm_stream1);
+                }
             }
             if(key_prod_perm_stream){
                 console.log(key_prod_perm_stream);
