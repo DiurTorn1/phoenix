2fa2cf2089acf43c8bca6561649981f2bedf0041
unknown <diurtorn@gmail.com>
2024-10-29 00:21:28 +0300

    [*]

--- public/js/get_video_nologin.js
+++ public/js/get_video_nologin.js
@@ -28,28 +28,50 @@ function paint_element_stream(){
     }
 
     for(var i = 0; i < count_stream; i ++){
-
-        //for(var i = 0; i < count_product; i ++){
-           // $.post('/php/get_product_table.php',{ table:public_product_perm[i] }, function(data1)  {
-                //var output1 = $.parseJSON(data1);
-               // $.each(output1,function(i,item1){
-                    //if(item1.id_stream){
-                        //console.log(item1.id_stream);
+        for(var i = 0; i < count_product; i ++){
+            $.post('/php/get_product_table.php',{ table:public_product_perm[i] }, function(data1)  {
+                var output1 = $.parseJSON(data1);
+                $.each(output1,function(i,item1){
+                    //if(item1.id_stream == id_stream_prod){
+                        console.log(item1.id_stream);
+                        //key_prod_perm_stream1 = 1;
                     //}
                     //console.log(id_stream_prod);
-                //});
+                });
                 //console.log(key_prod_perm_stream);
-            //});
-        //}
-
-
+            });
+            //if(key_prod_perm_stream1){
+                console.log(key_prod_perm_stream1);
+            //}
+        }
         $.post('/php/get_stream_id.php',{ id:array_stream[i] }, function(data)  {
             //console.log(data);
             var output = $.parseJSON(data);
             var list = output.data;
             var name_stream_gl = list.name;
             var id_stream_prod = list.id;
-
+            /*var key_prod_perm_stream = 0;
+            for(var i = 0; i < count_product; i ++){
+                var key_prod_perm_stream1 = 0;
+                $.post('/php/get_product_table.php',{ table:public_product_perm[i] }, function(data1)  {
+                    key_prod_perm_stream1 = 0;
+                    var output1 = $.parseJSON(data1);
+                    $.each(output1,function(i,item1){
+                        if(item1.id_stream == id_stream_prod){
+                            //console.log(item1.id_stream);
+                            key_prod_perm_stream1 = 1;
+                        }
+                        //console.log(id_stream_prod);
+                    });
+                    //console.log(key_prod_perm_stream);
+                });
+                //if(key_prod_perm_stream1){
+                    console.log(key_prod_perm_stream1);
+                //}
+            }
+            if(key_prod_perm_stream){
+                console.log(key_prod_perm_stream);
+            }*/
             
             //console.log("Video inform:\r\n");
             //console.log("id: " + list.id + "\r\nworkspace_id: " + list.workspace_id + "\r\nparent_id: " + list.parent_id + "\r\nname: " + list.name + "\r\nsubtitle: " + list.subtitle +
