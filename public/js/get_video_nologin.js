2ce61f0ccf50c4509f5f30194e71eea44121474a
unknown <diurtorn@gmail.com>
2024-10-29 00:22:43 +0300

    [*]

--- public/js/get_video_nologin.js
+++ public/js/get_video_nologin.js
@@ -26,24 +26,24 @@ function paint_element_stream(){
        
 
     }
+    for(var i = 0; i < count_product; i ++){
+        $.post('/php/get_product_table.php',{ table:public_product_perm[i] }, function(data1)  {
+            var output1 = $.parseJSON(data1);
+            $.each(output1,function(i,item1){
+                //if(item1.id_stream == id_stream_prod){
+                    console.log(item1.id_stream);
+                    //key_prod_perm_stream1 = 1;
+                //}
+                //console.log(id_stream_prod);
+            });
+            //console.log(key_prod_perm_stream);
+        });
+        //if(key_prod_perm_stream1){
+        //}
+    }
 
     for(var i = 0; i < count_stream; i ++){
-        for(var i = 0; i < count_product; i ++){
-            $.post('/php/get_product_table.php',{ table:public_product_perm[i] }, function(data1)  {
-                var output1 = $.parseJSON(data1);
-                $.each(output1,function(i,item1){
-                    //if(item1.id_stream == id_stream_prod){
-                        console.log(item1.id_stream);
-                        //key_prod_perm_stream1 = 1;
-                    //}
-                    //console.log(id_stream_prod);
-                });
-                //console.log(key_prod_perm_stream);
-            });
-            //if(key_prod_perm_stream1){
-                console.log(key_prod_perm_stream1);
-            //}
-        }
+
         $.post('/php/get_stream_id.php',{ id:array_stream[i] }, function(data)  {
             //console.log(data);
             var output = $.parseJSON(data);
