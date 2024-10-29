2ef3ca6c5886eb472c7797cdcf3bdcc894c7e5c5
unknown <diurtorn@gmail.com>
2024-10-29 23:42:30 +0300

    [*]

--- public/js/get_video_user.js
+++ public/js/get_video_user.js
@@ -73,14 +73,13 @@ function paint_element_stream(){
             name_stream_gl = list.name;
             var id_stream_prod = list.id;
             var key_prod_perm_stream = 0;
-            console.log(public_product_perm2);
-            //for(var i = 0; i < public_product_perm2.length; i++){
-                //var get_str = public_product_perm2[i];
-                //console.log(get_str.id_stream);
-                //if(get_str.id_stream == id_stream_prod){
-                   // key_prod_perm_stream = 1;
-               // }
-            //}
+            for(var i = 0; i < public_product_perm2.length; i++){
+                var get_str = public_product_perm2[i];
+                console.log(get_str.id_stream);
+                if(get_str.id_stream == id_stream_prod){
+                    key_prod_perm_stream = 1;
+                }
+            }
             //console.log("Video inform:\r\n");
             //console.log("id: " + list.id + "\r\nworkspace_id: " + list.workspace_id + "\r\nparent_id: " + list.parent_id + "\r\nname: " + list.name + "\r\nsubtitle: " + list.subtitle +
                 //"\r\ntype: " + list.type + "\r\nstreamkey: " + list.streamkey + "\r\nauto_start: " + list.auto_start + "\r\nprotected: " + list.protected + "\r\ntime_shift: " + list.time_shift); 
@@ -384,7 +383,7 @@ function get_stream_array(){
                             $.each(output1,function(i,item1){
                                 var struct_prod = {
                                     "table": prm_prod, 
-                                    "id_stream ": item1.id_stream 
+                                    "id_stream": item1.id_stream 
                                 };
                                 //if(item1.id_stream == id_stream_prod){
                                     //console.log(struct_prod);
