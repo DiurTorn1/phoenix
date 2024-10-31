dd65f6f4f67ba7dafbb473b0536cacd86eec6500
unknown <diurtorn@gmail.com>
2024-10-31 20:58:01 +0300

    [*]

--- public/js/player_stream.js
+++ public/js/player_stream.js
@@ -239,7 +239,7 @@ $(document).ready(function() {
                         $("#player_stream_get").append(
                             '<div class="index-live-item-video-2">' +
                                 '<div class="index-live-item-video-3">' +
-                                    '<div class="index-live-banner">' +
+                                    '<div class="index-live-banner-1">' +
                                         //'<button class="top-menu-btn-1" >Купить Подписку</button>' +
                                         //'<button class="top-menu-btn-1" >Купить билет</button>' +
                                         '<img src="' + item.poster.original + '">' +
