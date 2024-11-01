eeda68ee6a58554048836d329580826838baa39e
unknown <diurtorn@gmail.com>
2024-11-01 15:01:01 +0300

    [*]

--- public/js/get_video_user.js
+++ public/js/get_video_user.js
@@ -113,8 +113,11 @@ function paint_element_stream(){
                         //console.log(pars_data[1]);
                         var pars_time = pars_data[1].split(":");
                         if(pars_date[0] == dNow.getFullYear() && pars_date[1] == (dNow.getMonth()+1)){
-                            console.log(pars_date[2]);
-                            console.log('0'+dNow.getDate());
+                            if(pars_date[2] == dNow.getDate()){
+                                console.log(pars_date[2]);
+                                console.log('0'+dNow.getDate());
+                            }
+
                         }
                         var key_player = 0, key_player1 = 0;
                         for(var i = 0; i < public_product_perm2.length; i++){
