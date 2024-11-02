d72870cf59d0fb1a6b7153bb13138a9ce885a4c6
unknown <diurtorn@gmail.com>
2024-11-02 23:01:24 +0300

    [+]

--- public/php/python_send.php
+++ public/php/python_send.php
@@ -5,7 +5,7 @@ $options = array(
     'http' => array(
         'header'  => "Content-type: application/json\r\n",
         'method'  => 'POST',
-        'content' => json_encode(array('mail' => $_POST[''])),
+        'content' => json_encode(array('mail' => $_POST['mail'])),
     ),
 );
 $context  = stream_context_create($options);
