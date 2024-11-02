78fde66a22c9174b8e2255476f710130a116f19f
unknown <diurtorn@gmail.com>
2024-11-02 17:46:32 +0300

    [*]

--- public/script/send_mail.py
+++ public/script/send_mail.py
@@ -1,19 +1,19 @@
-    #!/usr/bin/python3
-    import smtplib
+#!/usr/bin/python3
+import smtplib
     
-    sender = 'tehnodir.d@mail.ru'
-    receivers = [ 'diur_tornbeornson@mail.ru']
+sender = 'tehnodir.d@mail.ru'
+receivers = [ 'diur_tornbeornson@mail.ru']
     
-    message = """From: From Person <tehnodir.d@mail.ru>
-    To: To Person <diur_tornbeornson@mail.ru>
-    Subject: SMTP e-mail test
+message = """From: From Person <tehnodir.d@mail.ru>
+To: To Person <diur_tornbeornson@mail.ru>
+Subject: SMTP e-mail test
     
-    This is a test e-mail message.
-    """
+This is a test e-mail message.
+"""
     
-    try:
-       smtpObj = smtplib.SMTP('localhost')
-       smtpObj.sendmail(sender, receivers, message)
-       print("Successfully sent email")
-    except SMTPException:
-       print("Error: unable to send email")
+try:
+    smtpObj = smtplib.SMTP('localhost')
+    smtpObj.sendmail(sender, receivers, message)
+    print("Successfully sent email")
+except SMTPException:
+    print("Error: unable to send email")
