484c1248b4599e9a5daacbd5a466f1fc57c05a32
unknown <diurtorn@gmail.com>
2024-11-02 23:17:25 +0300

    [+]

--- public/script/app.py
+++ public/script/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/process_data', methods=['POST'])
 def process_data():
     mail = request.json.get('mail')
-    if data:
+    if mail:
         processed_data = "Processed: " + mail
         print(processed_data)
         put = "Maksimalochka"
