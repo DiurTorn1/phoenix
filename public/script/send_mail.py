914326a7a020b14cfb14a43541a7928521c5cac1
unknown <diurtorn@gmail.com>
2024-11-02 16:48:43 +0300

    [*]

--- public/script/send_mail.py
+++ public/script/send_mail.py
@@ -69,7 +69,7 @@ msg.attach(MIMEText(html_message, 'html'))
 # Send the email
 try:
     # Use SMTP_SSL for secure connection
-    with smtplib.SMTP_SSL('smtp.localhost', 465) as server:
+    with smtplib.SMTP_SSL('localhost', 465) as server:
         server.login(sender_email, "190984Dilen!?")
         server.sendmail(sender_email, receiver_email, msg.as_string())
     print("Email sent successfully!")
