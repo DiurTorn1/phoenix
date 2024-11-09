a91cc78de245770c2630a71b97a59ea700b3f687
unknown <diurtorn@gmail.com>
2024-11-09 23:52:58 +0300

    [*]

--- public/php/users_finish_reg.php
+++ public/php/users_finish_reg.php
@@ -1,7 +1,7 @@
 <?php
 require "config_bd.php";
 
-$sql = "INSERT INTO `users`(`name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES ('". $_POST['name'] ."','". $_POST['email'] ."','','". $_POST['password'] ."','','','')";
+$sql = "INSERT INTO `users`(`name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES ('". $_POST['name'] ."','". $_POST['email'] ."','". $_POST['email_verified_at'] ."','". $_POST['password'] ."','". $_POST['remember_token'] ."','". $_POST['created_at'] ."','". $_POST['updated_at'] ."')";
 
 if ($conn->query($sql) === TRUE) {
   echo "OK";
