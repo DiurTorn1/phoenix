4e394c51661b402a56870b7972bc4952a4991f95
unknown <diurtorn@gmail.com>
2024-11-10 01:27:54 +0300

    [*]

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 		
         <title>@yield('title-block')</title>
 		<script type="text/javascript" src = "https://code.jquery.com/jquery-3.7.1.js"></script>
-		<script type="text/javascript" src = "{{ asset('js/bscrypt.js') }}"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bcryptjs/2.4.3/bcrypt.min.js"></script>
         <script type="text/javascript" src = "{{ asset('js/scrypt.js') }}"></script>
 		<script type="text/javascript" src = "{{ asset('js/Backbone.js') }}"></script>
 		
