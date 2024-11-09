b0389c96284b9f9ba584a271c147ac2672e55c31
unknown <diurtorn@gmail.com>
2024-11-10 00:43:46 +0300

    [*]

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
 		<script type="text/javascript" src = "https://code.jquery.com/jquery-3.7.1.js"></script>
         <script type="text/javascript" src = "{{ asset('js/scrypt.js') }}"></script>
 		<script type="text/javascript" src = "{{ asset('js/Backbone.js') }}"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/bcryptjs/2.4.3/bcrypt.min.js"></script>
+		<script type="text/javascript" src = "{{ asset('js/bcrypt.js') }}"></script>
 
         <!--<link rel="icon" href="{{ asset('img/logo_main.png') }}" class="top_img">-->
 		<link type="image/x-icon" href="{{ asset('img/favicon.png') }}" rel="shortcut icon">
