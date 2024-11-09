93efb443f91b056e5d22343010208fc10dc0a89f
unknown <diurtorn@gmail.com>
2024-11-10 02:19:50 +0300

    [*]

--- app/Http/Controllers/HashController.php
+++ app/Http/Controllers/HashController.php
@@ -6,16 +6,9 @@ class UserController extends Controller
 {
     public function store(Request $request)
     {
-        
-        // Validate the request data
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|string|min:8',
-        ]);
 
         // Create a new user
-        $user = 'password' => bcrypt($validatedData['password']);
+        $user = array('test' => 'test');
 
         // Return a response
         return response()->json($user, 201);
