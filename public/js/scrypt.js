7a5ea14c9d5f52e704989cb019b40fb332d216fc
unknown <diurtorn@gmail.com>
2024-11-09 23:44:29 +0300

    [*]

--- public/js/scrypt.js
+++ public/js/scrypt.js
@@ -141,6 +141,8 @@ $(document).ready(function() {
                     console.log(output[2]);
                     console.log(roles);
                     console.log(CryptoJS.MD5(output[2]));
+                    var md5 = $.md5(output[2], null, true);
+                    console.log(md5);
                     //$.post('/php/users_finish_reg.php', { name:split_email[0] ,email:output[1] ,email_verified_at:NULL ,password:output[2] ,remember_token:NULL ,created_at:NULL,updated_at:NULL }, function(data) {
                         //console.log(output);
                     //});
