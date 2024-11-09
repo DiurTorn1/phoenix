84e1a63a736eeb85bb8ed91921f6daefeca650f5
unknown <diurtorn@gmail.com>
2024-11-10 02:04:58 +0300

    [*]

---
+++ app/Http/Controllers/HashController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers;
+
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        
+
+        // Return a response
+        return 'Test';
+    }
+}
