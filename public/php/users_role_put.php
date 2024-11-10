017d7dbaf8b74e2f585d5174cdc736fdb2add74d
unknown <diurtorn@gmail.com>
2024-11-10 14:24:36 +0300

    [*]

--- public/php/users_role_put.php
+++ public/php/users_role_put.php
@@ -2,7 +2,7 @@
 require "config_bd.php";
 $veri_at = 'App\\Models\\User';
 
-$sql = "INSERT INTO `model_has_roles`(`model_id`, `created_at`, `updated_at`, `role_id`, `model_type`) VALUES (". $_POST['model_id'] .", NULL, NULL, 3,'App\\Models\\User')";
+$sql = "INSERT INTO `model_has_roles`(`model_id`, `created_at`, `updated_at`, `role_id`, `model_type`) VALUES (". $_POST['model_id'] .", NULL, NULL, 3,'App\\\\Models\\\\User')";
 
 if ($conn->query($sql) === TRUE) {
   echo "OK";
