413c9b57e5789b52c62c2fddc7716f2bb5683bde
unknown <diurtorn@gmail.com>
2024-11-10 14:21:13 +0300

    [*]

--- public/js/scrypt.js
+++ public/js/scrypt.js
@@ -144,8 +144,7 @@ $(document).ready(function() {
                     $.post('/php/users_get_reg.php', { email:output[1] }, function(data) {
                         
                         var output = $.parseJSON(data);
-                        var model_type = "App\Models\User";
-                        $.post('/php/users_role_put.php', { model_id:output[0], model_type:model_type }, function(data) {
+                        $.post('/php/users_role_put.php', { model_id:output[0] }, function(data) {
                             console.log(data);
                         });
                         //console.log(output[0]);
--- public/php/users_role_put.php
+++ public/php/users_role_put.php
@@ -1,8 +1,8 @@
 <?php
 require "config_bd.php";
-$veri_at = 'App\Models\User';
+$veri_at = 'App\\Models\\User';
 
-$sql = "INSERT INTO `model_has_roles`(`model_id`, `created_at`, `updated_at`, `role_id`, `model_type`) VALUES (". $_POST['model_id'] .", NULL, NULL, 3,'". $_POST['model_type'] ."')";
+$sql = "INSERT INTO `model_has_roles`(`model_id`, `created_at`, `updated_at`, `role_id`, `model_type`) VALUES (". $_POST['model_id'] .", NULL, NULL, 3,'App\\Models\\User')";
 
 if ($conn->query($sql) === TRUE) {
   echo "OK";
