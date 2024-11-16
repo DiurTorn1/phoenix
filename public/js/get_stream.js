c43c9881996ac79ddcf51672522a147be4f754d9
unknown <diurtorn@gmail.com>
2024-11-16 20:05:48 +0300

    [*]

--- public/js/get_stream.js
+++ public/js/get_stream.js
@@ -56,12 +56,12 @@ function dynamic_checkbox(){
             for(var k=0; k<array_name_stream.length;k++){
                 var name_list = array_name_stream[k];
                 if(name_list.id == arr_p){
-                    console.log(name_list.name);
+                            console.log(name_list.name);
                     
-                //for(var i = 0; i < public_stream_list.length; i++){
-                    //if(public_stream_list[i] == name_list.name){ console.log(public_stream_list[i]);}
+                    for(var j = 0; j < public_stream_list.length; j++){
+                        if(public_stream_list[i] == name_list.name){ console.log(public_stream_list[i]);}
                         ///console.log(public_stream_list);
-                    //}
+                    }
                    
                 }
             }
