a0814aa4315e8776a2e5e40501d1053e7bfbcb1a
unknown <diurtorn@gmail.com>
2024-11-16 18:43:47 +0300

    [*]

--- public/js/get_stream.js
+++ public/js/get_stream.js
@@ -36,10 +36,10 @@ function dynamic_checkbox(){
         if(ch1){
             var id = $("#admin-vl" + i).parent().parent().attr('id');
             //console.log(id);array_stream[i]
-            for(var i = 0; i < public_stream_list.length; i++){
-                //var name_list = public_stream_list[i];
-                //if(name_list.id == id){ console.log(name_list.name); }
-                console.log(public_stream_list[i]);
+            for(var i = 0; i < array_name_stream.length; i++){
+                var name_list = public_stream_list[i];
+                if(name_list.id == id){ console.log(name_list.name); }
+                //console.log(array_name_stream[i]);
             }
             int_ch++;
         }
