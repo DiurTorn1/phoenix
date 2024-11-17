8dc9e1e885a0674d981934d45125d2895ea8a46c
unknown <diurtorn@gmail.com>
2024-11-17 14:51:40 +0300

    [*]

--- public/php/get_product_table_point.php
+++ public/php/get_product_table_point.php
@@ -1,7 +1,7 @@
 <?php
 require "config_bd.php";
 
-$sql = "SELECT `id`, `id_product`, `type`, `create_at`, `initial` FROM `product_public_permission` WHERE id_product=".$_POST['id'];
+$sql = "SELECT `id`, `id_product`, `type`, `create_at`, `initial` FROM `product_public_permission` WHERE id_product='".$_POST['id']."'";
 
 $result = $conn->query($sql);
 $row = $result->fetch_row();
