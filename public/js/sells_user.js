f3d83ac660b70d0aa73177fb6d4efbcf447bfac7
unknown <diurtorn@gmail.com>
2024-11-19 13:26:28 +0300

    [*]

--- public/js/sells_user.js
+++ public/js/sells_user.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         //console.log(output);
         $.each(output,function(i,item){
             console.log(item.id_product);
-            user_arr_product.push(id_product)
+            user_arr_product.push(item.id_product)
             $.post('/php/get_sell_user_id.php',{product_id:item.id_product}, function(data1) {
                 var output1 = $.parseJSON(data1);
                 console.log(output1);
