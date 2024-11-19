8e49f15a85bd07b3c1e3bd44c11f57a025038ef8
unknown <diurtorn@gmail.com>
2024-11-19 15:14:15 +0300

    [*]

--- public/js/sells_user.js
+++ public/js/sells_user.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
         var count_sub = 0, count_tick = 0;
         $.each(output,function(i,item){
             var key_sell = 0;
-            console.log(item.id);
+            //console.log(item.id);
             for(var k=0;k<user_arr_sell.length;k++){
                 var sell_user = user_arr_sell[i];
                 console.log(sell_user.id_product);
@@ -42,11 +42,11 @@ $(document).ready(function() {
                 
             }
             for(var i=0; i<user_arr_product.length;i++){
-                console.log(user_arr_product[i]);
+                //console.log(user_arr_product[i]);
                 if(item.id == user_arr_product[i]){
                     var pars_detail = item.detail.split("+");
                     if(pars_detail[0] == 'ticket'){
-                        count_sub++;
+                        count_tick++;
                         //console.log(item.name + '   ^   '+ key_sell);
                         $("#sell_ticket_user").append(
                             '<div class="admin-video-prev">' +
@@ -60,7 +60,7 @@ $(document).ready(function() {
                         );
 
                     }else if(pars_detail[0] == 'subscript'){
-                        count_tick++;
+                        count_sub++;
                         //console.log(item.name + '   ^   '+ key_sell);
                         $("#sell_subs_user").append(
                             '<div class="admin-video-prev">' +
@@ -76,30 +76,24 @@ $(document).ready(function() {
 
                 }
             }
-            console.log(count_sub + "  ^  " + count_tick);
-            //var int_id = parseInt(item.id_product);
-            //user_arr_sell.push({email:params.get('user_email'), id_product:int_id})
-            /**
-             *                         $("#product_list_add").append(
-                            '<li class="admin-video-item admin-back" id="' + output[0] + '">' +
-                            '<div class="admin-video-prev">' +
-                                '<img src="' + output[17] + '" alt="" class="" style="heigth: 100%; width: 100%">' +
-                            '</div>' +
-                            '<div class="admin-video-item-header">' +
-                                '<div class="admin-video-item-h-list">' +
-                                    '<span class="admin-video-item-h-date">' + output[1] + '</span>' +
-                                '</div>' +
-                                '<div class="admin-video-item-h-hashtag">' +
-                                    '<span class="admin-prod-hashtag">' + output[2] + '</span>' +
-                                '</div>' +
-                                '<div class="admin-video-item-info admin-video-item-view">' +
-                                    '<button class="button_prod_dyn" id="' + output[0] + '">Добавить</button>' +
-                                '</div>' +
-                            '</div>' +
-                        '</li>');
-
-							
-             */
+            if(!count_sub){
+                $("#sell_ticket_user").append(
+                    '<div class="admin-video-item-header">' +
+                        '<div class="admin-video-item-h-list">' +
+                            '<span class="admin-video-item-h-date">У вас не куплено ни одного билета</span>'+
+                        '</div>' +
+                    '</div>'
+                );
+            }
+            if(!count_tick){
+                $("#sell_ticket_user").append(
+                    '<div class="admin-video-item-header">' +
+                        '<div class="admin-video-item-h-list">' +
+                            '<span class="admin-video-item-h-date">У вас не куплено ни одной подписки</span>'+
+                        '</div>' +
+                    '</div>'
+                );
+            }
         });
     });
 });
