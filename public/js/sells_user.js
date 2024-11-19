3874e4b49ced1216adc3d874f9cca52dcd43fcfb
unknown <diurtorn@gmail.com>
2024-11-19 12:36:36 +0300

    [*]

--- public/js/sells_user.js
+++ public/js/sells_user.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
     var user_sells = Array();
-    $.post('/php/get_sell_user.php', function(data) {
+    var params = new window.URLSearchParams(window.location.search);
+
+    $.post('/php/get_sell_user.php',{email:params.get('user_email')}, function(data) {
         var output = $.parseJSON(data);
         console.log(output);
         //$.each(output,function(i,item){
             //console.log(item.id_product);
         //});
     });
-    var params = new window.URLSearchParams(window.location.search);
+
     //console.log(params.get('user_email'));
     $.post('/php/get_product_all.php', function(data) {
         var output = $.parseJSON(data);
