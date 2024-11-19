2adffd04bc5261920934b152dc9185118438eb68
unknown <diurtorn@gmail.com>
2024-11-19 12:35:54 +0300

    [*]

--- public/js/sells_user.js
+++ public/js/sells_user.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+    var user_sells = Array();
+    $.post('/php/get_sell_user.php', function(data) {
+        var output = $.parseJSON(data);
+        console.log(output);
+        //$.each(output,function(i,item){
+            //console.log(item.id_product);
+        //});
+    });
     var params = new window.URLSearchParams(window.location.search);
     //console.log(params.get('user_email'));
     $.post('/php/get_product_all.php', function(data) {
