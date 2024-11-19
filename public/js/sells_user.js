ebe821796e5b02133f97ffbf7767813d1a1cfd23
unknown <diurtorn@gmail.com>
2024-11-19 12:39:56 +0300

    [*]

--- public/js/sells_user.js
+++ public/js/sells_user.js
@@ -1,15 +1,24 @@
-$(document).ready(function() {
-    var user_sells = Array();
-    var params = new window.URLSearchParams(window.location.search);
+var user_sells = Array();
+var user_gl_name;
 
-    $.post('/php/get_sell_user.php',{email:params.get('user_email')}, function(data) {
+function paint_element_stream(){
+    $.post('/php/get_sell_user.php',{email:user_gl_name}, function(data) {
         var output = $.parseJSON(data);
         console.log(output);
+        user_sells.push(output);
         //$.each(output,function(i,item){
             //console.log(item.id_product);
         //});
     });
+}
 
+$(document).ready(function() {
+    
+    var params = new window.URLSearchParams(window.location.search);
+    user_gl_name = params.get('user_email');
+
+
+    console.log(user_sells);
     //console.log(params.get('user_email'));
     $.post('/php/get_product_all.php', function(data) {
         var output = $.parseJSON(data);
