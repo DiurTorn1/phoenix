e7171cb9e83ffec5a507037efda5eb3130c23aee
unknown <diurtorn@gmail.com>
2024-11-19 13:17:34 +0300

    [*]

--- public/js/sells_user.js
+++ public/js/sells_user.js
@@ -1,27 +1,30 @@
 $(document).ready(function() {
     var params = new window.URLSearchParams(window.location.search);
+    var user_arr_sell = new Array();
 
     $.post('/php/get_sell_user.php',{user_email:params.get('user_email')}, function(data) {
         var output = $.parseJSON(data);
-        console.log(output);
-        //$.each(output,function(i,item){
-            //console.log(item.id_product);
-        //});
+        //console.log(output);
+        $.each(output,function(i,item){
+            console.log(item.id_product);
+            user_arr_sell.push({email:params.get('user_email'), id_product:item.id_product})
+        });
     });
-
+    
     //console.log(params.get('user_email'));
     $.post('/php/get_product_all.php', function(data) {
         var output = $.parseJSON(data);
         //console.log(output);
         $.each(output,function(i,item){
             console.log(item.id_product);
-            $.post('/php/get_sell_user_id.php',{product_id:item.id_product}, function(data1) {
-                var output1 = $.parseJSON(data1);
-                console.log(output1);
+            console.log(user_arr_sell);
+           // $.post('/php/get_sell_user_id.php',{product_id:item.id_product}, function(data1) {
+                //var output1 = $.parseJSON(data1);
+                //console.log(output1);
                 //$.each(output,function(i,item){
                     //console.log(item.id_product);
                 //});
-            });
+            //});
         });
     });
 });
