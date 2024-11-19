2d0828bdcfbfed95fef529bf564ff9fa52bf4d2f
unknown <diurtorn@gmail.com>
2024-11-19 13:09:01 +0300

    [*]

--- public/js/sells_user.js
+++ public/js/sells_user.js
@@ -1,29 +1,34 @@
 $(document).ready(function() {
     var params = new window.URLSearchParams(window.location.search);
-var key_sell = 0;
+
     //console.log(params.get('user_email'));
     $.post('/php/get_product_all.php', function(data) {
         var output = $.parseJSON(data);
         //console.log(output);
         $.each(output,function(i,item){
             //console.log(item.id_product);
+            var key_sell = 0;
+            (async function() {
+                var globalVariable;
             
-            $.post('/php/get_sell_user_id.php',{product_id:item.id_product}, function(data1) {
-                var output1 = $.parseJSON(data1);  
-                var key_sell1 = 0;
-                if(output1[2]==params.get('user_email')){
-                    key_sell=1;
-                    console.log(output1);
-                }else{
-                    var key_sell = 0;
+                try {
+                    const response = await $.ajax({
+                        url: '/php/get_sell_user_id.php',
+                        method: 'POST',
+                        data: { product_id:item.id_product }
+                    });
+                    var output1 = $.parseJSON(response); 
+                    if(output1[2]==params.get('user_email')){
+                        key_sell=1;
+                    }
+                    console.log('Global Variable:', output1);
+            
+                    // Выполняем нужные действия с globalVariable
+                    doSomethingWithGlobalVariable(globalVariable);
+                } catch (error) {
+                    console.error('AJAX Error:', error);
                 }
-                //$.each(output,function(i,item){
-                    //console.log(item.id_product);
-                //});
-                key_sell = key_sell1;
-            }).fail(function(jqXHR, textStatus, errorThrown) {
-                console.error('AJAX Error:', textStatus, errorThrown);
-            });
+            })();
             console.log(key_sell);
         });
     });
