4a27cb23425afe193f03053d9a740648c73c3e85
unknown <diurtorn@gmail.com>
2024-11-19 12:53:28 +0300

    [*]

--- public/js/sells_user.js
+++ public/js/sells_user.js
@@ -1,29 +1,24 @@
 $(document).ready(function() {
     var params = new window.URLSearchParams(window.location.search);
 
-    $.post('/php/get_sell_user_id.php',{product_id:58}, function(data) {
-        var output = $.parseJSON(data);
-        console.log(output);
-        //$.each(output,function(i,item){
-            //console.log(item.id_product);
-        //});
-    });
-
     //console.log(params.get('user_email'));
     $.post('/php/get_product_all.php', function(data) {
         var output = $.parseJSON(data);
         //console.log(output);
         $.each(output,function(i,item){
-            console.log(item.id_product);
+            //console.log(item.id_product);
+            var key_sell = 0;
             $.post('/php/get_sell_user_id.php',{product_id:item.id_product}, function(data1) {
                 var output1 = $.parseJSON(data1);  
                 if(output1[2]==params.get('user_email')){
+                    key_sell=1;
                     console.log(output1);
                 }
                 //$.each(output,function(i,item){
                     //console.log(item.id_product);
                 //});
             });
+            console.log(key_sell);
         });
     });
 });
