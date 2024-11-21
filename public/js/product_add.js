1bb4b7d4d6d2ffed3101ffa625ef7ed36038e1cf
unknown <diurtorn@gmail.com>
2024-11-21 20:47:16 +0300

    [*]

--- public/js/product_add.js
+++ public/js/product_add.js
@@ -285,6 +285,7 @@ $(document).ready(function() {
         if(!input_old_prace_bilet){ $("#input_old_prace_bilet").addClass("user-input-err"); arr_bilet.push(0); } else { $("#input_old_prace_bilet").removeClass("user-input-err"); arr_bilet.push(1); }
         if(!price_bilet){ $("#prace_bilet").addClass("user-input-err"); arr_bilet.push(0); } else { $("#prace_bilet").removeClass("user-input-err"); arr_bilet.push(1); }
         var key_bil = 0
+        console.log(arr_bilet);
         var allEqual1 = arr_bilet.every(function(value, index, arr) {
             //console.log(value);
             //console.log(arr[0]);
