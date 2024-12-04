e1ebdd6dbab166fe78fd43583862829fb9b27f24
unknown <diurtorn@gmail.com>
2024-12-04 17:32:28 +0300

    [*]

--- public/js/sell_panel.js
+++ public/js/sell_panel.js
@@ -88,7 +88,7 @@ $(document).ready(function() {
         console.log(localdate);
         var status = 'load';
         console.log(ip_port + ' ' + user_sell + ' ' + name_product + ' ' + res_price + ' ' + id_product + ' ' + status + ' ' + localdate );
-        $.post('/php/presell_payment.php', {ip_port:ip_port, mail:mail, name_product:name_product, price:price, id_product:id_product, status:status, created_at:localdate}, function(data)  {
+        $.post('/php/presell_payment.php', {ip_port:ip_port, mail:user_sell, name_product:name_product, price:price, id_product:id_product, status:status, created_at:localdate}, function(data)  {
             //console.log(data);
             if(data == 'OK'){
                 $.post('/php/sell_user_payment.php', {name_product:name_product, price_product:res_price, id_product:id_product}, function(data)  {
