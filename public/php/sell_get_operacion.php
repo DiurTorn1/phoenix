62010b425642b77b95ec7e49d3eb97d61594ba81
unknown <diurtorn@gmail.com>
2024-12-05 15:34:12 +0300

    [*]

--- public/php/sell_get_operacion.php
+++ public/php/sell_get_operacion.php
@@ -6,7 +6,7 @@ $invoiceID = 7814822027;
 $password2 = 'Z8fL6IuzbiJ7kq8K1xRb'; // Пароль#2 из технических настроек
 
 // Генерация контрольной суммы (Signature)
-$signature = md5($merchantLogin . ':' . $invoiceID . ':' . $password2);
+$signature = md5("$merchantLogin:$invoiceID:$password2");
 
 // Формирование URL запроса
 $url = "https://auth.robokassa.ru/Merchant/WebService/Service.asmx/OpStateExt?MerchantLogin={$merchantLogin}&InvoiceID={$invoiceID}&Signature={$signature}";
