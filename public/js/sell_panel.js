8294c03289ba5e91992b4e5b0949994e8ab78dbc
unknown <diurtorn@gmail.com>
2024-12-09 23:41:44 +0300

    [*]

--- public/js/sell_panel.js
+++ public/js/sell_panel.js
@@ -90,21 +90,21 @@ $(document).ready(function() {
         console.log(ip_port + ' ' + user_sell + ' ' + name_product + ' ' + res_price + ' ' + id_product + ' ' + status + ' ' + localdate );
         $.post('/php/users_get_reg.php', {email:user_sell}, function(get_reg)  {
             var get_pars = $.parseJSON(get_reg);
-            console.log(get_pars);
+            console.log(get_pars[0]);
             if(get_pars){
-                var id_pay = 1000000000 + id_product + get_pars[0];
-                $.post('/php/presell_payment.php', {ip_port:ip_port, mail:user_sell, name_product:name_product, price:res_price, id_product:id_pay, status:status, created_at:localdate}, function(data)  {
+                //var id_pay = 1000000000 + parseInt(id_product) + get_pars[0];
+                //$.post('/php/presell_payment.php', {ip_port:ip_port, mail:user_sell, name_product:name_product, price:res_price, id_product:id_pay, status:status, created_at:localdate}, function(data)  {
                     //console.log(data);
-                    if(data == 'OK'){
-                        $.post('/php/sell_user_payment.php', {name_product:name_product, price_product:res_price, id_product:id_pay}, function(data)  {
+                    //if(data == 'OK'){
+                        //$.post('/php/sell_user_payment.php', {name_product:name_product, price_product:res_price, id_product:id_pay}, function(data)  {
                                 //console.log(data);
-                            window.location.href=data;
-                            window.location.href="/";
-                        });
-                    }
+                            //window.location.href=data;
+                            //window.location.href="/";
+                        //});
+                    //}
                    // window.location.href=data;
                    // window.location.href="/"; ip_port mail name_product price id_product created_at status
-                });
+                //});
             }
         });
 
