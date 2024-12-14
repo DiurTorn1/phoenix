afb5fe7b4b9f6a096d4062f7a1dfeb98ddcaffef
unknown <diurtorn@gmail.com>
2024-12-14 22:54:30 +0300

    [*]

--- public/js/presell.js
+++ public/js/presell.js
@@ -3,36 +3,39 @@ $(document).ready(function(){
         var output = $.parseJSON(data);
         //console.log(output);
         $.each(output,function(i,item){
-            if(item.status == 'load'){
-                //console.log(item.id + ' : ' + item.id_product + ' : ' + item.ip_port + ' : ' + item.mail + ' : ' + item.name_product + ' : ' + item.price + ' : ' + item.status + ' : ' + item.created_at);
-                $("#presell_list").append(
-                    '<tr class="tr-wrapper admin-video-item admin-back">' +
-                        '<td class="paid-amount">' + item.price + ' &#8381;</td>' +
-                        '<td class="">' + item.name_product + '</td>' +
-                        '<td class="table-one-column">' +
-                            '<a href="#" class="paid-users">' + item.mail + '</a>' +
-                        '</td>' +
-                        '<td class="table-one-column disp-fl-sb fl-dir-col">' + item.created_at + '</td>' +
-                        '<td class="td-item-action">' +
-                            '<button class="del-button del_sell" id="' + item.id + '">Удалить</button>' +
-                            '<button class="del-button ver_sell" id="' + item.id + '">Подтвердить подписку</button>' +
-                        '</td>' +
-                    '</tr>'); 
-            } else {
-                $("#presell_list").append(
-                    '<tr class="tr-wrapper admin-video-item admin-back">' +
-                        '<td class="paid-amount">' + item.price + ' &#8381;</td>' +
-                        '<td class="">' + item.name_product + '</td>' +
-                        '<td class="table-one-column">' +
-                            '<a href="#" class="paid-users">' + item.mail + '</a>' +
-                        '</td>' +
-                        '<td class="table-one-column disp-fl-sb fl-dir-col">' + item.created_at + '</td>' +
-                        '<td class="td-item-action">' +
-                            '<button class="del-button del_sell" id="' + item.id + '">Удалить</button>' +
-                            '<button class="del-button ver_sell" id="' + item.id + '">Отменить подписку</button>' +
-                        '</td>' +
-                    '</tr>');  
+            if(item.name_product == 'Тестовая на 3 дня'){
+                if(item.status == 'load'){
+                    //console.log(item.id + ' : ' + item.id_product + ' : ' + item.ip_port + ' : ' + item.mail + ' : ' + item.name_product + ' : ' + item.price + ' : ' + item.status + ' : ' + item.created_at);
+                    $("#presell_list").append(
+                        '<tr class="tr-wrapper admin-video-item admin-back">' +
+                            '<td class="paid-amount">' + item.price + ' &#8381;</td>' +
+                            '<td class="">' + item.name_product + '</td>' +
+                            '<td class="table-one-column">' +
+                                '<a href="#" class="paid-users">' + item.mail + '</a>' +
+                            '</td>' +
+                            '<td class="table-one-column disp-fl-sb fl-dir-col">' + item.created_at + '</td>' +
+                            '<td class="td-item-action">' +
+                                '<button class="del-button del_sell" id="' + item.id + '">Удалить</button>' +
+                                '<button class="del-button ver_sell" id="' + item.id + '">Подтвердить подписку</button>' +
+                            '</td>' +
+                        '</tr>'); 
+                } else {
+                    $("#presell_list").append(
+                        '<tr class="tr-wrapper admin-video-item admin-back">' +
+                            '<td class="paid-amount">' + item.price + ' &#8381;</td>' +
+                            '<td class="">' + item.name_product + '</td>' +
+                            '<td class="table-one-column">' +
+                                '<a href="#" class="paid-users">' + item.mail + '</a>' +
+                            '</td>' +
+                            '<td class="table-one-column disp-fl-sb fl-dir-col">' + item.created_at + '</td>' +
+                            '<td class="td-item-action">' +
+                                '<button class="del-button del_sell" id="' + item.id + '">Удалить</button>' +
+                                '<button class="del-button ver_sell" id="' + item.id + '">Отменить подписку</button>' +
+                            '</td>' +
+                        '</tr>');  
+                }
             }
+
         });
     });
 
