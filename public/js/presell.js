734fc66b4588c1aaf3880ab5849b201ed16b0af3
unknown <diurtorn@gmail.com>
2024-12-14 21:05:57 +0300

    [*]

--- public/js/presell.js
+++ public/js/presell.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){  
     $.post('/php/get_presells.php', function(data)  {
         var output = $.parseJSON(data);
-        console.log(output);
+        //console.log(output);
+        $.each(output,function(i,item){
+            if(item.status == 'load'){
+                console.log(output);
+            }
+        });
     });
 });
