d9afc89447a49d80da29e23e140f82fa5345f47b
unknown <diurtorn@gmail.com>
2025-02-24 21:55:44 +0300

    [*]

--- public/php/get_subscribe_line_time.php
+++ public/php/get_subscribe_line_time.php
@@ -4,8 +4,8 @@ require "config_bd.php";
 $sql = "SELECT `id`, `id_sell`, `id_prod`, `status`, `time_end`, `create_at` FROM `subscribe_line` WHERE time_end='".$_POST['time_end']."'";
 
 $result = $conn->query($sql);
-$row = $result->fetch_row();
-echo json_encode($row);//$row[0] . "&" . $row[1] . "&" . $row[2] . "&" . $row[3];
+$news_events = $result->fetch_all(MYSQLI_ASSOC);
+echo json_encode($news_events);//$row[0] . "&" . $row[1] . "&" . $row[2] . "&" . $row[3];
 //if ($conn->query($sql) === TRUE) {
   //echo $conn->query($sql);
 //} else {
