8d687c1ad7bca480f525b52448e16e230f7e3fa5
unknown <diurtorn@gmail.com>
2025-02-24 12:14:52 +0300

    [*]

--- public/php/all_subscribe_add.php
+++ public/php/all_subscribe_add.php
@@ -1,7 +1,7 @@
 <?php
 require "config_bd.php";
 
-$sql = "INSERT INTO `all_subscribe`(`user_email`, `id_sell`, `period`, `create_at`) VALUES ('". $_POST['email'] ."','". $_POST['id_sell'] ."','". $_POST['period'] ."','". $_POST['create_at'] ."')";
+$sql = "INSERT INTO `all_subscribe`(`user_email`, `id_sell`, `period`, `create_at`) VALUES ('". $_POST['user_email'] ."','". $_POST['id_sell'] ."','". $_POST['period'] ."','". $_POST['create_at'] ."')";
 
 if ($conn->query($sql) === TRUE) {
   echo "OK";
