8cf31578eeca778a7004c03e0f391416f7156a52
unknown <diurtorn@gmail.com>
2025-02-24 22:00:20 +0300

    [*]

--- public/js/scrypt.js
+++ public/js/scrypt.js
@@ -174,9 +174,9 @@ function subscribe_line(){
     //console.log(howTime);
     $.post('/php/get_subscribe_line_time.php', {time_end:howTime }, function(data_sl) {
         var res_data = $.parseJSON(data_sl);
-        //if(res_data != null){
+        if(res_data != null){
             console.log(res_data);
-        //}
+        }
         
     });
 }
