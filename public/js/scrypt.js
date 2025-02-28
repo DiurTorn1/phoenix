33c7fdfa3bdb3876b00d523f1d3f7fdb9e2a140c
unknown <diurtorn@gmail.com>
2025-02-28 14:12:38 +0300

    [*]

--- public/js/scrypt.js
+++ public/js/scrypt.js
@@ -294,12 +294,16 @@ function subscribe_line(){
                                         var res_prod_fin = $.parseJSON(data_prod_fin);
                                         //console.log(res_prod_fin[1]);
                                         $.post('/php/reccurent_payment_load.php',{summ:res_subs_all[11], name_prod:res_prod_fin[1], id_new:res_item.id_prod, id_Inv:res_item.id_sell }, function(data_send) {   
-                                            console.log(data_send);
-                                            //if (data_send === 'Ошибка при обработке платежа.') {
-                                                //console.error('Ошибка при отправке данных');
-                                            //} else {
-                                                
-                                            //}
+                                            //console.log(data_send);
+                                            if (data_send === 'Ошибка при обработке платежа.') {
+                                                console.error('Ошибка при обработке платежа.');
+                                            } else {
+                                                $.post('/php/upload_all_subscribe_status.php', {id_sell:length_id_sell, status:'load'}, function(data_ps) {
+                                                    if(data_ps == "OK"){
+                                                        console.log("Повторный платёж обработан.");
+                                                    } 
+                                                });
+                                            }
                                         });
                                     });
                                 });
