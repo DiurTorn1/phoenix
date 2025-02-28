f66924a92029b2027d08143786e908bb2da375c8
unknown <diurtorn@gmail.com>
2025-02-28 16:10:55 +0300

    [*]

--- public/php/reccurent_payment_load.php
+++ public/php/reccurent_payment_load.php
@@ -1,12 +1,24 @@
 <?php
+// Включение вывода ошибок
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $merchantLogin = 'phoenixtv.ru'; // Логин магазина
-$outSum = $_POST['summ']; // Сумма платежа
+$outSum = floatval($_POST['summ']); // Сумма платежа
 $invoiceID = $_POST['id_new']; // Новый номер счета
-$description = $_POST['name_prod']; // Описание
+$description = trim($_POST['name_prod']); // Описание
 $previousInvoiceID = $_POST['id_Inv']; // Номер первого счета в серии
 $secretKey = 'nCWY57iaB1fKog5zr2BK'; // Пароль 1 из Robokassa
 
-//echo $outSum . ' ' . $invoiceID . ' ' . $description . ' ' . $previousInvoiceID;
+// Проверка суммы платежа
+if ($outSum <= 0) {
+    die("Ошибка: сумма платежа должна быть больше 0.");
+}
+
+// Очистка описания
+$description = htmlspecialchars($description, ENT_QUOTES, 'UTF-8');
+
 // Генерация подписи (без PreviousInvoiceID)
 $signatureValue = md5("$merchantLogin:$outSum:$invoiceID:$secretKey");
 
@@ -47,13 +59,8 @@ $verboseLog = stream_get_contents($verbose);
 fclose($verbose);
 file_put_contents('curl_log.txt', $verboseLog);
 
-if ($response) {
-    echo "Ответ от Robokassa: " . htmlspecialchars($response);
-} else {
-    echo "Ответ от Robokassa пустой.";
-}
 // Обработка ответа
-/*if ($response) {
+if ($response) {
     if (strpos($response, 'Оплата успешно выполнена') !== false) {
         echo "Платеж успешно обработан.";
     } else {
@@ -61,5 +68,5 @@ if ($response) {
     }
 } else {
     echo "Ошибка при отправке запроса.";
-}*/
+}
 ?>
