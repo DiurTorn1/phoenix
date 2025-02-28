ce57f3d125a269606b5a1b1df22f5ee7dbf73d2c
unknown <diurtorn@gmail.com>
2025-02-28 15:50:44 +0300

    [*]

--- public/php/reccurent_payment_load.php
+++ public/php/reccurent_payment_load.php
@@ -21,22 +21,38 @@ $requestData = [
     'SignatureValue' => $signatureValue,
 ];
 
+// Логирование параметров запроса
+file_put_contents('request_data.txt', print_r($requestData, true));
+
 // Отправка запроса через cURL
 $ch = curl_init('https://auth.robokassa.ru/Merchant/Recurring');
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($requestData));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
+// Логирование cURL
+curl_setopt($ch, CURLOPT_VERBOSE, true);
+$verbose = fopen('php://temp', 'w+');
+curl_setopt($ch, CURLOPT_STDERR, $verbose);
+
 $response = curl_exec($ch);
 curl_close($ch);
 
+// Логирование ответа
+file_put_contents('robokassa_response.html', $response);
+
+// Логирование cURL
+rewind($verbose);
+$verboseLog = stream_get_contents($verbose);
+fclose($verbose);
+file_put_contents('curl_log.txt', $verboseLog);
+
 // Обработка ответа
 if ($response) {
-    // Robokassa возвращает HTML-страницу, поэтому парсим её (пример)
     if (strpos($response, 'Оплата успешно выполнена') !== false) {
         echo "Платеж успешно обработан.";
     } else {
-        echo "Ошибка при обработке платежа.";
+        echo "Ошибка при обработке платежа. Ответ сервера: " . htmlspecialchars($response);
     }
 } else {
     echo "Ошибка при отправке запроса.";
